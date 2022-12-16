cc91cfcf57d70e1f16f49703655ca2e6c024bbe9
tuyucheng <tuyucheng2000@163.com>
2022-12-16 11:10:18 +0800

    ✨ Java中的组合设计模式

---
+++ design-patterns-modules/design-patterns-structural/src/main/java/cn/tuyucheng/taketoday/composite/SalesDepartment.java
@@ -0,0 +1,35 @@
+package cn.tuyucheng.taketoday.composite;
+
+/**
+ * Created by Tuyucheng
+ */
+public class SalesDepartment implements Department {
+
+	private Integer id;
+	private String name;
+
+	public SalesDepartment(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public void printDepartmentName() {
+		System.out.println(getClass().getSimpleName());
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
