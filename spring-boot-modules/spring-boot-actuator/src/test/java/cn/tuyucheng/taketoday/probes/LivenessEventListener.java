1de24b885e92592737ade450f25b1fef1366d6a3
tuyucheng <tuyucheng2000@163.com>
2022-12-19 20:06:38 +0800

    ✨ 如何在Spring Boot Actuator中启用所有端点

---
+++ spring-boot-modules/spring-boot-actuator/src/test/java/cn/tuyucheng/taketoday/probes/LivenessEventListener.java
@@ -0,0 +1,19 @@
+package cn.tuyucheng.taketoday.probes;
+
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.context.event.EventListener;
+
+public class LivenessEventListener {
+
+	@EventListener
+	public void onEvent(AvailabilityChangeEvent<LivenessState> event) {
+		switch (event.getState()) {
+			case BROKEN:
+				// notify others
+				break;
+			case CORRECT:
+				// we're back
+		}
+	}
+}
