ea0f1286f36bbacfd803bd82e7f5243a586d0130
tuyucheng <tuyucheng2000@163.com>
2022-12-20 17:34:37 +0800

    ✨ 使用Spring Boot创建自定义自动配置

---
+++ spring-boot-modules/spring-boot-autoconfiguration/README.md
@@ -0,0 +1,11 @@
+## Spring Boot Auto Configuration
+
+本模块包含有关Spring Boot自动配置的文章
+
+## 相关文章
+
++ [使用Spring Boot创建自定义自动配置](docs/使用SpringBoot创建自定义自动配置.md)
++ []()
++ []()
++ []()
++ []()
---
+++ spring-boot-modules/spring-boot-autoconfiguration/src/main/java/cn/tuyucheng/taketoday/autoconfiguration/example/MyUser.java
@@ -0,0 +1,26 @@
+package cn.tuyucheng.taketoday.autoconfiguration.example;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class MyUser {
+	@Id
+	private String email;
+
+	public MyUser() {
+	}
+
+	public MyUser(String email) {
+		super();
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
