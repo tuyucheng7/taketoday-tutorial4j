7cae05cd1ae57e2eb0bb3809b0e8873dda17f7ea
tuyucheng <tuyucheng2000@163.com>
2022-12-25 21:16:32 +0800

    ✨ 使用Spring Boot自动扩展属性

---
+++ spring-boot-modules/spring-boot-property-exp/property-exp-default-config/src/main/java/cn/tuyucheng/taketoday/propertyexpansion/components/PropertyLoggerBean.java
@@ -0,0 +1,33 @@
+package cn.tuyucheng.taketoday.propertyexpansion.components;
+
+import cn.tuyucheng.taketoday.jacoco.exclude.annotations.ExcludeFromJacocoGeneratedReport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@ExcludeFromJacocoGeneratedReport
+public class PropertyLoggerBean {
+
+	private static final Logger log = LoggerFactory.getLogger(PropertyLoggerBean.class);
+
+	@Value("${expanded.project.version}")
+	private String projectVersion;
+
+	@Value("${expanded.project.property}")
+	private String projectProperty;
+
+	@PostConstruct
+	public void printProperties() {
+		log.info("");
+		log.info("Properties logged from logger bean");
+		log.info("===========================================");
+		log.info("Project version : {}", projectVersion);
+		log.info("Project property : {}", projectProperty);
+		log.info("===========================================");
+		log.info("");
+	}
+}
