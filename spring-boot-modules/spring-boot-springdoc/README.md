cf49eb9dedafb547bb1207a0c22c683c7b27a0a9
tuyucheng <tuyucheng2000@163.com>
2022-12-27 00:49:52 +0800

    ✨ Spring REST Docs与OpenAPI

--- spring-boot-modules/spring-boot-springdoc/README.md
+++ spring-boot-modules/spring-boot-springdoc/README.md
@@ -1,7 +1,7 @@
 ## 相关文章
 
 + [使用Open API 3.0记录Spring REST API](docs/使用OpenAPI3.0记录SpringRESTAPI.md)
-+ []()
++ [Spring REST Docs与OpenAPI](docs/Spring-RESTDocs与OpenAPI.md)
 + []()
 + []()
 + []()
---
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/cn/tuyucheng/taketoday/restdocopenapi/FooRepository.java
@@ -0,0 +1,9 @@
+package cn.tuyucheng.taketoday.restdocopenapi;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FooRepository extends PagingAndSortingRepository<Foo, Long> {
+
+}
