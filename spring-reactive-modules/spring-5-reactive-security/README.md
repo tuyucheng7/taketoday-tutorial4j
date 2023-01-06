f76b8210d8851d0144e34a045d69542759435db7
tuyucheng <tuyucheng2000@163.com>
2023-01-06 16:08:34 +0800

    ✨ Spring Webflux和CORS

--- spring-reactive-modules/spring-5-reactive-security/README.md
+++ spring-reactive-modules/spring-5-reactive-security/README.md
@@ -6,3 +6,4 @@
 
 + [Spring Boot Actuator](docs/SpringBoot-Actuator.md)
 + [Spring Security中的AuthenticationManagerResolver指南](docs/SpringSecurity中的AuthenticationManagerResolver指南.md)
++ [Spring Webflux和CORS](docs/Spring-Webflux和CORS.md)
---
+++ spring-reactive-modules/spring-5-reactive-security/src/main/java/cn/tuyucheng/taketoday/reactive/cors/webfilter/functional/routers/CorsWithWebFilterRouterFunctions.java
@@ -0,0 +1,19 @@
+package cn.tuyucheng.taketoday.reactive.cors.webfilter.functional.routers;
+
+import cn.tuyucheng.taketoday.reactive.cors.webfilter.functional.handlers.CorsWithWebFilterHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class CorsWithWebFilterRouterFunctions {
+
+	@Bean
+	public RouterFunction<ServerResponse> corsWebfilterRouter(@Autowired CorsWithWebFilterHandler handler) {
+		return RouterFunctions.route(RequestPredicates.PUT("/web-filter-on-functional/functional-endpoint"), handler::useHandler);
+	}
+}
