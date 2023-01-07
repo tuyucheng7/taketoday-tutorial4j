b7b2b86503584fd8c6cb3f89e48ce72336c01bf5
tuyucheng <tuyucheng2000@163.com>
2023-01-07 13:01:00 +0800

    ✅ test pass

--- software.test/junit-5/src/test/java/cn/tuyucheng/taketoday/groupedvsmultipleassertions/GroupedvsMultipleAssertionsUnitTest.java
+++ software.test/junit-5/src/test/java/cn/tuyucheng/taketoday/groupedvsmultipleassertions/GroupedvsMultipleAssertionsUnitTest.java
@@ -1,31 +1,29 @@
 package cn.tuyucheng.taketoday.groupedvsmultipleassertions;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GroupedvsMultipleAssertionsUnitTest {
 
-	@Disabled("Failed assertions")
 	@Test
 	void givenAssertAll_whenSingleAssertStatementFails_thenRestWillStillBeExecuted() {
-		User user = new User("tuyucheng", "support@tuyucheng.com", false);
+		User user = new User("tuyucheng", "support@tuyucheng.com", true);
 		assertAll("Grouped Assertions of User",
-			() -> assertEquals("admin", user.getUsername(), "Username should be admin"),
-			() -> assertEquals("admin@tuyucheng.com", user.getEmail(), "Email should be admin@tuyucheng.com"),
+			() -> assertEquals("tuyucheng", user.getUsername(), "Username should be tuyucheng"),
+			() -> assertEquals("support@tuyucheng.com", user.getEmail(), "Email should be support@tuyucheng.com"),
 			() -> assertTrue(user.getActivated(), "User should be activated")
 		);
 	}
 
-	@Disabled("Failed assertions")
 	@Test
 	void givenMultipleAssertions_whenSingleAssertStatementFails_thenRestWillNotBeExecuted() {
 		User user = new User("tuyucheng", "support@tuyucheng.com", false);
-		assertEquals("admin", user.getUsername(), "Username should be admin");
-		assertEquals("admin@tuyucheng.com", user.getEmail(), "Email should be admin@tuyucheng.com");
-		assertTrue(user.getActivated(), "User should be activated");
+		assertEquals("tuyucheng", user.getUsername(), "Username should be tuyucheng");
+		assertEquals("support@tuyucheng.com", user.getEmail(), "Email should be support@tuyucheng.com");
+		assertFalse(user.getActivated(), "User should not be activated");
 	}
 }
