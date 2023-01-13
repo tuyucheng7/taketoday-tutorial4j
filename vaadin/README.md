cff7779fd45b5072e2755d132bec08e537315278
tuyucheng <tuyucheng2000@163.com>
2023-01-13 13:01:42 +0800

    🎉 新建子模块"vaadin"

---
+++ vaadin/README.md
@@ -0,0 +1,8 @@
+## Vaadin
+
+本模块包含有关Vaadin的文章。
+
+## 相关文章
+
++ [Vaadin简介](docs/Vaadin简介.md)
++ [使用Spring Boot和Vaadin的示例应用程序](docs/使用SpringBoot和Vaadin的示例应用程序.md)
---
+++ vaadin/src/main/java/cn/tuyucheng/taketoday/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package cn.tuyucheng.taketoday;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	List<Employee> findByLastNameStartsWithIgnoreCase(String lastName);
+}
