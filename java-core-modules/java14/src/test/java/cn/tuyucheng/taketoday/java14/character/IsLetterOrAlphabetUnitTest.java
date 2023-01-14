c21c6afc4cd3c3a337881af29952202f8de7ea76
tuyucheng <tuyucheng2000@163.com>
2023-01-14 23:38:28 +0800

    🎉 新建子模块"java14"

---
+++ java-core-modules/java14/src/test/java/cn/tuyucheng/taketoday/java14/character/IsLetterOrAlphabetUnitTest.java
@@ -0,0 +1,29 @@
+package cn.tuyucheng.taketoday.java14.character;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class IsLetterOrAlphabetUnitTest {
+
+	@Test
+	public void givenACharacter_whenLetter_thenAssertIsLetterTrue() {
+		assertTrue(Character.isLetter(65));
+	}
+
+	@Test
+	public void givenACharacter_whenLetter_thenAssertIsAlphabeticTrue() {
+		assertTrue(Character.isAlphabetic(65));
+	}
+
+	@Test
+	public void givenACharacter_whenAlphabeticAndNotLetter_thenAssertIsLetterFalse() {
+		assertFalse(Character.isLetter(837));
+	}
+
+	@Test
+	public void givenACharacter_whenAlphabeticAndNotLetter_thenAssertIsAlphabeticTrue() {
+		assertTrue(Character.isAlphabetic(837));
+	}
+}
---
+++ java-core-modules/java14/src/test/java/cn/tuyucheng/taketoday/java14/newfeatues/MultilineUnitTest.java
@@ -0,0 +1,25 @@
+package cn.tuyucheng.taketoday.java14.newfeatues;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MultilineUnitTest {
+
+	@SuppressWarnings("preview")
+	String multiline = """
+		   A quick brown fox jumps over a lazy dog; \
+		the lazy dog howls loudly.""";
+
+	@SuppressWarnings("preview")
+	String anotherMultiline = """
+		   A quick brown fox jumps over a lazy dog;
+		the lazy dog howls loudly.""";
+
+	@Test
+	public void givenMultilineString_whenSlashUsed_thenNoNewLine() {
+		assertFalse(multiline.contains("\n"));
+		assertTrue(anotherMultiline.contains("\n"));
+	}
+}
