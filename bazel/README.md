6a51744cd32895e8cc50eccfa0e987783cc653a2
tuyucheng <tuyucheng2000@163.com>
2023-01-17 20:28:02 +0800

    🎉 新建子模块"bazel"

---
+++ bazel/README.md
@@ -0,0 +1,7 @@
+## Bazel
+
+本模块包含有关Bazel的文章
+
+## 相关文章
+
++ [使用Bazel构建Java应用程序](docs/使用Bazel构建Java应用程序.md)
---
+++ bazel/bazelgreeting/src/main/java/cn/tuyucheng/taketoday/Greetings.java
@@ -0,0 +1,8 @@
+package cn.tuyucheng.taketoday;
+
+public class Greetings {
+
+	public String greet(String name) {
+		return "Hello ".concat(name);
+	}
+}
