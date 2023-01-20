7b892283a6a479cdafd94da6db6807c65bbfe370
tuyucheng <tuyucheng2000@163.com>
2023-01-20 21:19:33 +0800

    ♻️ JUnit 5

--- spring-modules/spring-activiti/src/test/java/cn/tuyucheng/taketoday/SpringContextTest.java
+++ spring-modules/spring-activiti/src/test/java/cn/tuyucheng/taketoday/SpringContextTest.java
@@ -1,18 +1,15 @@
 package cn.tuyucheng.taketoday;
 
 import cn.tuyucheng.taketoday.activitiwithspring.ActivitiWithSpringApplication;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(classes = ActivitiWithSpringApplication.class)
 @AutoConfigureTestDatabase
-public class SpringContextTest {
+class SpringContextTest {
 
 	@Test
-	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+	void whenSpringContextIsBootstrapped_thenNoExceptions() {
 	}
 }
