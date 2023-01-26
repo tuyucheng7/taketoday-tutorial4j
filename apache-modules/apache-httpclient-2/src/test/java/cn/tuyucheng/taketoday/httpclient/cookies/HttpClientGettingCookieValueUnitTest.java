4049c07b803ad996fafe399aa40492f3c7524c06
tuyucheng <tuyucheng2000@163.com>
2023-01-26 16:37:00 +0800

    ⬆️ Upgraded to apache http client 5.2

--- apache-modules/apache-httpclient-2/src/test/java/cn/tuyucheng/taketoday/httpclient/cookies/HttpClientGettingCookieValueUnitTest.java
+++ apache-modules/apache-httpclient-2/src/test/java/cn/tuyucheng/taketoday/httpclient/cookies/HttpClientGettingCookieValueUnitTest.java
@@ -1,56 +1,60 @@
 package cn.tuyucheng.taketoday.httpclient.cookies;
 
-import org.apache.http.client.CookieStore;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.cookie.ClientCookie;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.cookie.BasicClientCookie;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.cookie.BasicCookieStore;
+import org.apache.hc.client5.http.cookie.Cookie;
+import org.apache.hc.client5.http.cookie.CookieStore;
+
+import org.apache.hc.client5.http.impl.classic.BasicHttpClientResponseHandler;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+
+import org.apache.hc.client5.http.impl.cookie.BasicClientCookie;
+import org.apache.hc.client5.http.protocol.HttpClientContext;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+class HttpClientGettingCookieValueUnitTest {
+    private static Logger log = LoggerFactory.getLogger(HttpClientGettingCookieValueUnitTest.class);
 
+    private static final String SAMPLE_URL = "http://www.baeldung.com/";
 
-public class HttpClientGettingCookieValueUnitTest {
-	private static Logger log = LoggerFactory.getLogger(HttpClientGettingCookieValueUnitTest.class);
+    @Test
+    void whenSettingCustomCookieOnTheRequest_thenGettingTheSameCookieFromTheResponse() throws IOException {
 
-	private static final String SAMPLE_URL = "http://www.baeldung.com/";
+        HttpClientContext context = HttpClientContext.create();
+        context.setAttribute(HttpClientContext.COOKIE_STORE, createCustomCookieStore());
 
-	@Test
-	public final void whenSettingCustomCookieOnTheRequest_thenGettingTheSameCookieFromTheResponse() throws IOException {
-		HttpClientContext context = HttpClientContext.create();
-		context.setAttribute(HttpClientContext.COOKIE_STORE, createCustomCookieStore());
+        final HttpGet request = new HttpGet(SAMPLE_URL);
 
-		try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-			try (CloseableHttpResponse response = httpClient.execute(new HttpGet(SAMPLE_URL), context)) {
-				CookieStore cookieStore = context.getCookieStore();
-				Cookie customCookie = cookieStore.getCookies()
-					.stream()
-					.peek(cookie -> log.info("cookie name:{}", cookie.getName()))
-					.filter(cookie -> "custom_cookie".equals(cookie.getName()))
-					.findFirst()
-					.orElseThrow(IllegalStateException::new);
+        try (CloseableHttpClient client = HttpClientBuilder.create()
+              .build()) {
+            client.execute(request, context, new BasicHttpClientResponseHandler());
+            CookieStore cookieStore = context.getCookieStore();
+            Cookie customCookie = cookieStore.getCookies()
+                  .stream()
+                  .peek(cookie -> log.info("cookie name:{}", cookie.getName()))
+                  .filter(cookie -> "custom_cookie".equals(cookie.getName()))
+                  .findFirst()
+                  .orElseThrow(IllegalStateException::new);
 
-				assertEquals("test_value", customCookie.getValue());
-			}
-		}
-	}
+            assertEquals("test_value", customCookie.getValue());
+        }
+    }
 
-	private BasicCookieStore createCustomCookieStore() {
-		BasicCookieStore cookieStore = new BasicCookieStore();
-		BasicClientCookie cookie = new BasicClientCookie("custom_cookie", "test_value");
-		cookie.setDomain("baeldung.com");
-		cookie.setAttribute(ClientCookie.DOMAIN_ATTR, "true");
-		cookie.setPath("/");
-		cookieStore.addCookie(cookie);
-		return cookieStore;
-	}
+    private BasicCookieStore createCustomCookieStore() {
+        BasicCookieStore cookieStore = new BasicCookieStore();
+        BasicClientCookie cookie = new BasicClientCookie("custom_cookie", "test_value");
+        cookie.setDomain("baeldung.com");
+        cookie.setAttribute("domain", "true");
+        cookie.setPath("/");
+        cookieStore.addCookie(cookie);
+        return cookieStore;
+    }
 }
