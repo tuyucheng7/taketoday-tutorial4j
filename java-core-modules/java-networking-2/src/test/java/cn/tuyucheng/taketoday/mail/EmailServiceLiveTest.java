4b84e0324e2a2e634252edae99e2eb1a74e23957
tuyucheng <tuyucheng2000@163.com>
2023-01-30 19:58:09 +0800

    ♻️ Update Java Mail articles

--- java-core-modules/java-networking-2/src/test/java/cn/tuyucheng/taketoday/mail/EmailServiceLiveTest.java
+++ java-core-modules/java-networking-2/src/test/java/cn/tuyucheng/taketoday/mail/EmailServiceLiveTest.java
@@ -1,68 +1,70 @@
 package cn.tuyucheng.taketoday.mail;
 
-import com.icegreen.greenmail.junit.GreenMailRule;
-import com.icegreen.greenmail.util.ServerSetupTest;
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import java.io.IOException;
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.ServerSetupTest;
 
-import static org.junit.Assert.assertEquals;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 
 public class EmailServiceLiveTest {
 
-    @Rule
-    public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP);
+	@Rule
+	public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP);
 
-    private EmailService emailService;
+	private EmailService emailService;
 
-    @Before
-    public void setup() {
-        emailService = new EmailService("localhost", greenMail.getSmtp().getPort());
-    }
+	@Before
+	public void setup() {
+		emailService = new EmailService("localhost", greenMail.getSmtp().getPort());
+	}
 
-    @Test
-    public void givenEmailMessageWithAttachment_whenEmailIsSent_MessageIsReceived() throws Exception {
+	@Test
+	public void givenEmailMessageWithAttachment_whenEmailIsSent_MessageIsReceived() throws Exception {
 
-        emailService.sendMail();
+		emailService.sendMail();
 
-        MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
-        assertEquals(1, receivedMessages.length);
+		MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
+		assertEquals(1, receivedMessages.length);
 
-        MimeMessage receivedMessage = receivedMessages[0];
-        assertEquals("Mail Subject", subjectFromMessage(receivedMessage));
-        assertEquals("This is my first email using JavaMailer", emailTextFrom(receivedMessage));
-        assertEquals("This is my <b style='color:red;'>bold-red email</b> using JavaMailer", emailStyledTextFrom(receivedMessage));
-        assertEquals("sample attachment content", attachmentContentsFrom(receivedMessage));
-    }
+		MimeMessage receivedMessage = receivedMessages[0];
+		assertEquals("Mail Subject", subjectFromMessage(receivedMessage));
+		assertEquals("This is my first email using JavaMailer", emailTextFrom(receivedMessage));
+		assertEquals("This is my <b style='color:red;'>bold-red email</b> using JavaMailer", emailStyledTextFrom(receivedMessage));
+		assertEquals("sample attachment content", attachmentContentsFrom(receivedMessage));
+	}
 
-    private static String subjectFromMessage(MimeMessage receivedMessage) throws MessagingException {
-        return receivedMessage.getSubject();
-    }
+	private static String subjectFromMessage(MimeMessage receivedMessage) throws MessagingException {
+		return receivedMessage.getSubject();
+	}
 
-    private static String emailTextFrom(MimeMessage receivedMessage) throws IOException, MessagingException {
-        return ((MimeMultipart) receivedMessage.getContent())
-              .getBodyPart(0)
-              .getContent()
-              .toString();
-    }
+	private static String emailTextFrom(MimeMessage receivedMessage) throws IOException, MessagingException {
+		return ((MimeMultipart) receivedMessage.getContent())
+			.getBodyPart(0)
+			.getContent()
+			.toString();
+	}
 
-    private static String emailStyledTextFrom(MimeMessage receivedMessage) throws IOException, MessagingException {
-        return ((MimeMultipart) receivedMessage.getContent())
-              .getBodyPart(1)
-              .getContent()
-              .toString();
-    }
+	private static String emailStyledTextFrom(MimeMessage receivedMessage) throws IOException, MessagingException {
+		return ((MimeMultipart) receivedMessage.getContent())
+			.getBodyPart(1)
+			.getContent()
+			.toString();
+	}
 
-    private static String attachmentContentsFrom(MimeMessage receivedMessage) throws Exception {
-        return ((MimeMultipart) receivedMessage.getContent())
-              .getBodyPart(2)
-              .getContent()
-              .toString();
-    }
+	private static String attachmentContentsFrom(MimeMessage receivedMessage) throws Exception {
+		return ((MimeMultipart) receivedMessage.getContent())
+			.getBodyPart(2)
+			.getContent()
+			.toString();
+	}
 
 }
--- java-core-modules/java-networking-2/src/test/java/cn/tuyucheng/taketoday/mail/mailwithattachment/MailWithAttachmentServiceLiveTest.java
+++ java-core-modules/java-networking-2/src/test/java/cn/tuyucheng/taketoday/mail/mailwithattachment/MailWithAttachmentServiceLiveTest.java
@@ -1,77 +1,75 @@
 package cn.tuyucheng.taketoday.mail.mailwithattachment;
 
-import com.icegreen.greenmail.configuration.GreenMailConfiguration;
-import com.icegreen.greenmail.junit.GreenMailRule;
-import com.icegreen.greenmail.util.GreenMailUtil;
-import com.icegreen.greenmail.util.ServerSetupTest;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import javax.annotation.Resource;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetupTest;
 
-import static org.junit.Assert.assertEquals;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
 
 public class MailWithAttachmentServiceLiveTest {
 
-    private static final String USERNAME = "testUser";
-    private static final String PASSWORD = "password";
-    private static final String HOSTNAME = "localhost";
+	private static final String USERNAME = "testUser";
+	private static final String PASSWORD = "password";
+	private static final String HOSTNAME = "localhost";
 
-    @Rule
-    public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP)
-          .withConfiguration(
-                GreenMailConfiguration.aConfig()
-                      .withUser(USERNAME, PASSWORD)
-          );
+	@Rule
+	public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.SMTP)
+		.withConfiguration(
+			GreenMailConfiguration.aConfig()
+				.withUser(USERNAME, PASSWORD)
+		);
 
-    @Resource
-    private MailWithAttachmentService emailService;
+	private MailWithAttachmentService emailService;
 
-    @Before
-    public void setup() {
-        emailService = new MailWithAttachmentService(
-              USERNAME, PASSWORD, HOSTNAME, greenMail.getSmtp().getPort()
-        );
-    }
+	@Before
+	public void setup() {
+		emailService = new MailWithAttachmentService(
+			USERNAME, PASSWORD, HOSTNAME, greenMail.getSmtp().getPort()
+		);
+	}
 
-    @Test
-    public void givenEmailService_whenMessageSentWithAttachments_thenMessageIsReceived() throws Exception {
+	@Test
+	public void givenEmailService_whenMessageSentWithAttachments_thenMessageIsReceived() throws Exception {
 
-        Session tlsSession = emailService.getSession();
-        emailService.sendMail(tlsSession);
+		Session tlsSession = emailService.getSession();
+		emailService.sendMail(tlsSession);
 
-        MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
-        assertEquals(1, receivedMessages.length);
+		MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
+		assertEquals(1, receivedMessages.length);
 
-        MimeMessage receivedMessage = receivedMessages[0];
-        assertEquals("Testing Subject", subjectFrom(receivedMessage));
-        assertEquals("This is message body", emailTextFrom(receivedMessage));
-        assertEquals("sample attachment content", attachment1ContentsFrom(receivedMessage));
-        assertEquals("sample attachment content 2", attachment2ContentsFrom(receivedMessage));
-    }
+		MimeMessage receivedMessage = receivedMessages[0];
+		assertEquals("Testing Subject", subjectFrom(receivedMessage));
+		assertEquals("This is message body", emailTextFrom(receivedMessage));
+		assertEquals("sample attachment content", attachment1ContentsFrom(receivedMessage));
+		assertEquals("sample attachment content 2", attachment2ContentsFrom(receivedMessage));
+	}
 
-    private static String subjectFrom(MimeMessage receivedMessage) throws MessagingException {
-        return receivedMessage.getSubject();
-    }
+	private static String subjectFrom(MimeMessage receivedMessage) throws MessagingException {
+		return receivedMessage.getSubject();
+	}
 
-    private static String emailTextFrom(MimeMessage receivedMessage) throws Exception {
-        return GreenMailUtil.getBody(((MimeMultipart) receivedMessage.getContent())
-              .getBodyPart(0));
-    }
+	private static String emailTextFrom(MimeMessage receivedMessage) throws Exception {
+		return GreenMailUtil.getBody(((MimeMultipart) receivedMessage.getContent())
+			.getBodyPart(0));
+	}
 
-    private static String attachment1ContentsFrom(MimeMessage receivedMessage) throws Exception {
-        return GreenMailUtil.getBody(((MimeMultipart) receivedMessage.getContent())
-              .getBodyPart(1));
-    }
+	private static String attachment1ContentsFrom(MimeMessage receivedMessage) throws Exception {
+		return GreenMailUtil.getBody(((MimeMultipart) receivedMessage.getContent())
+			.getBodyPart(1));
+	}
 
-    private static String attachment2ContentsFrom(MimeMessage receivedMessage) throws Exception {
-        return GreenMailUtil.getBody(((MimeMultipart) receivedMessage.getContent())
-              .getBodyPart(2));
-    }
-
+	private static String attachment2ContentsFrom(MimeMessage receivedMessage) throws Exception {
+		return GreenMailUtil.getBody(((MimeMultipart) receivedMessage.getContent())
+			.getBodyPart(2));
+	}
 }
