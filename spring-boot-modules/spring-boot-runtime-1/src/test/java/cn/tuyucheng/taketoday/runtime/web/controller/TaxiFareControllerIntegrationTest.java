1c08caf7c57e9129e6a2f839c2a829f1b4624098
tuyucheng <tuyucheng2000@163.com>
2023-02-01 11:47:15 +0800

    ✅ update test case

--- spring-boot-modules/spring-boot-runtime-1/src/test/java/cn/tuyucheng/taketoday/runtime/web/controller/TaxiFareControllerIntegrationTest.java
+++ spring-boot-modules/spring-boot-runtime-1/src/test/java/cn/tuyucheng/taketoday/runtime/web/controller/TaxiFareControllerIntegrationTest.java
@@ -1,47 +1,33 @@
-package cn.tuyucheng.taketoday.runtime.web.controller;
+package cn.tuyucheng.taketoday.web.controller;
 
-import cn.tuyucheng.taketoday.runtime.web.log.app.Application;
-import cn.tuyucheng.taketoday.runtime.web.log.data.TaxiRide;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import cn.tuyucheng.taketoday.web.log.app.Application;
+import cn.tuyucheng.taketoday.web.log.data.TaxiRide;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {Application.class, TaxiFareControllerIntegrationTest.SecurityConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class TaxiFareControllerIntegrationTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TaxiFareControllerIntegrationTest {
 
 	@LocalServerPort
 	private int port;
 
 	@Test
-	void givenRequest_whenFetchTaxiFareRateCard_thanOK() {
-		System.out.println(port);
+	public void givenRequest_whenFetchTaxiFareRateCard_thanOK() {
 		String URL = "http://localhost:" + port + "/spring-rest";
 		TestRestTemplate testRestTemplate = new TestRestTemplate();
-		TaxiRide taxiRide = new TaxiRide(true, 10L);
-		String fare = testRestTemplate.postForObject(URL + "/taxifare/calculate/", taxiRide, String.class);
+		TaxiRide taxiRide = new TaxiRide(true, 10l);
+		String fare = testRestTemplate.postForObject(
+			URL + "/taxifare/calculate/",
+			taxiRide, String.class);
 
 		assertThat(fare, equalTo("200"));
-	}
-
-	@Configuration
-	static class SecurityConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			System.out.println("security being set");
-			http.authorizeRequests()
-				.anyRequest().permitAll()
-				.and()
-				.csrf().disable();
-		}
 	}
 }
