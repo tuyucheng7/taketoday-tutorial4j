4812340d47efac6dddbcefdaa0013b6f26c777d2
tuyucheng <tuyucheng2000@163.com>
2023-02-02 18:37:38 +0800

    ✅ update tests to use junit5, mark live test

--- spring-data-modules/spring-data-redis/src/test/java/cn/tuyucheng/taketoday/SpringContextTest.java
+++ spring-data-modules/spring-data-redis/src/test/java/cn/tuyucheng/taketoday/SpringContextTest.java
@@ -1,31 +1,32 @@
 package cn.tuyucheng.taketoday;
 
-import cn.tuyucheng.taketoday.spring.data.redis.SpringRedisApplication;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+
+import cn.tuyucheng.taketoday.spring.data.redis.SpringRedisApplication;
+
 import redis.embedded.RedisServerBuilder;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = SpringRedisApplication.class)
 @DirtiesContext(classMode = ClassMode.BEFORE_CLASS)
-public class SpringContextTest {
+public class SpringContextLiveTest {
 
 	private static redis.embedded.RedisServer redisServer;
 
-	@BeforeClass
+	@BeforeAll
 	public static void startRedisServer() {
 		redisServer = new RedisServerBuilder().port(6379).setting("maxmemory 256M").build();
 		redisServer.start();
 	}
 
-	@AfterClass
+	@AfterAll
 	public static void stopRedisServer() {
 		redisServer.stop();
 	}
-
 	@Test
 	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
 	}
