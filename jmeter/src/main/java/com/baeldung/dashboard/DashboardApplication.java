cb5d7c3d382622d146965457fbd92ce0f5f1995b
tuyucheng <tuyucheng2000@163.com>
2023-02-07 10:12:46 +0800

    🔨 Added JMeter-Dashboard profile to isolate

--- jmeter/src/main/java/com/baeldung/dashboard/DashboardApplication.java
+++ jmeter/src/main/java/com/baeldung/dashboard/DashboardApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;import org.springframework.context.annotation.Profile;
 
+@Profile("JMeter-Dashboard")
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, MongoAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class DashboardApplication {
     public static void main(String[] args) throws Exception {
