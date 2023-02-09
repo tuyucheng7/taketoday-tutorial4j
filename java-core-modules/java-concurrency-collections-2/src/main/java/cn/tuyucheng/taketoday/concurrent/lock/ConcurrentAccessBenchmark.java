77df5fd16b3034d6b39c5451be14bba4e8953ddc
tuyucheng <tuyucheng2000@163.com>
2023-02-09 15:14:59 +0800

    ⚗️ Correct/Rename THREADS to TASKS and int threads to int tasks.

--- java-core-modules/java-concurrency-collections-2/src/main/java/cn/tuyucheng/taketoday/concurrent/lock/ConcurrentAccessBenchmark.java
+++ java-core-modules/java-concurrency-collections-2/src/main/java/cn/tuyucheng/taketoday/concurrent/lock/ConcurrentAccessBenchmark.java
@@ -1,6 +1,13 @@
 package cn.tuyucheng.taketoday.concurrent.lock;
 
-import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +19,7 @@ import java.util.concurrent.TimeUnit;
 @Warmup(iterations = 0)
 public class ConcurrentAccessBenchmark {
 	static final int SLOTS = 4;
-	static final int THREADS = 10000;
+	static final int TASKS = 10000;
 	static final int BUCKETS = Runtime.getRuntime().availableProcessors() * SLOTS;
 	SingleLock singleLock = new SingleLock();
 	StripedLock stripedLock = new StripedLock(BUCKETS);
@@ -21,27 +28,27 @@ public class ConcurrentAccessBenchmark {
 	@BenchmarkMode(Mode.Throughput)
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
 	public Map<String, String> singleLockHashMap() throws InterruptedException {
-		return singleLock.doWork(new HashMap<>(), THREADS, SLOTS);
+		return singleLock.doWork(new HashMap<>(), TASKS, SLOTS);
 	}
 
 	@Benchmark
 	@BenchmarkMode(Mode.Throughput)
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
 	public Map<String, String> stripedLockHashMap() throws InterruptedException {
-		return stripedLock.doWork(new HashMap<>(), THREADS, SLOTS);
+		return stripedLock.doWork(new HashMap<>(), TASKS, SLOTS);
 	}
 
 	@Benchmark
 	@BenchmarkMode(Mode.Throughput)
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
 	public Map<String, String> singleLockConcurrentHashMap() throws InterruptedException {
-		return singleLock.doWork(new ConcurrentHashMap<>(), THREADS, SLOTS);
+		return singleLock.doWork(new ConcurrentHashMap<>(), TASKS, SLOTS);
 	}
 
 	@Benchmark
 	@BenchmarkMode(Mode.Throughput)
 	@OutputTimeUnit(TimeUnit.MILLISECONDS)
 	public Map<String, String> stripedLockConcurrentHashMap() throws InterruptedException {
-		return stripedLock.doWork(new ConcurrentHashMap<>(), THREADS, SLOTS);
+		return stripedLock.doWork(new ConcurrentHashMap<>(), TASKS, SLOTS);
 	}
 }
