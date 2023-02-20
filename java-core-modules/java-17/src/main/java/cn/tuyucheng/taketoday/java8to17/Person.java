b3b5fe657e39bca127a439a79f6dbfe59d7be37f
tuyucheng <tuyucheng2000@163.com>
2023-02-20 12:55:27 +0800

    ✨ Migrate from Java 8 to Java 17 + tests

---
+++ java-core-modules/java-17/src/main/java/cn/tuyucheng/taketoday/java8to17/Person.java
@@ -0,0 +1,29 @@
+package cn.tuyucheng.taketoday.java8to17;
+
+public class Person {
+
+	private String name;
+	private Address address;
+
+	public Person(String name, Address address) {
+		super();
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
---
+++ java-core-modules/java-17/src/main/java/cn/tuyucheng/taketoday/java8to17/Student.java
@@ -0,0 +1,4 @@
+package cn.tuyucheng.taketoday.java8to17;
+
+public record Student(int rollNo, String name) {
+}
