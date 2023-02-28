f355dc9ffea28465b5e4a691b3957f19eac1157f
tuyucheng <tuyucheng2000@163.com>
2023-02-28 19:06:18 +0800

    ✨ Resolving Jackson JsonMappingException: Can not deserialize instance of java.lang.String out of START_OBJECT token

--- jackson-modules/jackson-exceptions/src/test/java/cn/tuyucheng/taketoday/exceptions/JacksonExceptionsUnitTest.java
+++ jackson-modules/jackson-exceptions/src/test/java/cn/tuyucheng/taketoday/exceptions/JacksonExceptionsUnitTest.java
@@ -1,5 +1,16 @@
 package cn.tuyucheng.taketoday.exceptions;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonFactory;
@@ -11,14 +22,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
-import org.junit.Test;
-
-import java.io.IOException;
-import java.util.List;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 
 public class JacksonExceptionsUnitTest {
 
@@ -74,6 +77,32 @@ public class JacksonExceptionsUnitTest {
 		mapper.reader()
 			.forType(UserWithNoDefaultConstructor.class)
 			.readValue(json);
+	}
+
+	@Test
+	public void givenJsonObject_whenDeserializingIntoString_thenException() throws IOException {
+		final String json = "{\"firstName\":\"Azhrioun\",\"lastName\":\"Abderrahim\",\"contact\":{\"email\":\"azh@email.com\"}}";
+		final ObjectMapper mapper = new ObjectMapper();
+
+		Exception exception = assertThrows(JsonMappingException.class, () -> mapper.reader()
+			.forType(Person.class)
+			.readValue(json));
+
+		assertTrue(exception.getMessage()
+			.contains("Cannot deserialize value of type `java.lang.String` from Object value (token `JsonToken.START_OBJECT`)"));
+	}
+
+	@Test
+	public void givenJsonObject_whenDeserializingIntoObject_thenDeserialize() throws IOException {
+		final String json = "{\"firstName\":\"Azhrioun\",\"lastName\":\"Abderrahim\",\"contact\":{\"email\":\"azh@email.com\"}}";
+		final ObjectMapper mapper = new ObjectMapper();
+
+		PersonContact person = mapper.reader()
+			.forType(PersonContact.class)
+			.readValue(json);
+
+		assertEquals("azh@email.com", person.getContact()
+			.getEmail());
 	}
 
 	@Test
