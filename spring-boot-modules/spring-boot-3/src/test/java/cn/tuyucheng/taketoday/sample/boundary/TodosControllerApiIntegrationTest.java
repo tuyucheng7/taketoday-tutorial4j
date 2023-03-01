36b6d56b6da849ee6407494612c082065b8f6934
tuyucheng <tuyucheng2000@163.com>
2023-03-01 10:32:21 +0800

    ♻️ Migrate Application from Spring Boot 2 to Spring Boot 3

--- spring-boot-modules/spring-boot-3/src/test/java/cn/tuyucheng/taketoday/sample/boundary/TodosControllerApiIntegrationTest.java
+++ spring-boot-modules/spring-boot-3/src/test/java/cn/tuyucheng/taketoday/sample/boundary/TodosControllerApiIntegrationTest.java
@@ -203,4 +203,16 @@ class TodosControllerApiIntegrationTest {
 		mvc.perform(delete(BASEURL + "/5").contentType(DEFAULT_MEDIA_TYPE))
 			.andExpect(status().isNotFound());
 	}
+
+	@Test
+	void whenThereIsNoSlashMatching_ThenHttpStatusIs404() throws Exception {
+		mvc.perform(get(BASEURL + "/name/").contentType(DEFAULT_MEDIA_TYPE))
+			.andExpect(status().isNotFound());
+	}
+
+	@Test
+	void whenThereIsNoSlashMatching_ThenHttpStatusIs200() throws Exception {
+		mvc.perform(get(BASEURL + "/name").contentType(DEFAULT_MEDIA_TYPE))
+			.andExpect(status().isOk());
+	}
 }
