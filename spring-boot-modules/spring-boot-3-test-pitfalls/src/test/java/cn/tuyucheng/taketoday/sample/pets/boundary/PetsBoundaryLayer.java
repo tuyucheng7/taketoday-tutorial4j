7abdc7fe804eec21b2f1ba8a6c79f2db59620e35
tuyucheng <tuyucheng2000@163.com>
2023-03-03 21:48:26 +0800

    🚚 rename package

---
+++ spring-boot-modules/spring-boot-3-test-pitfalls/src/test/java/cn/tuyucheng/taketoday/sample/pets/boundary/PetsBoundaryLayer.java
@@ -0,0 +1,10 @@
+package cn.tuyucheng.taketoday.sample.pets.boundary;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Just an interface to use for compiler-checked component scanning during tests.
+ * @see ComponentScan#basePackageClasses()
+ */
+public interface PetsBoundaryLayer {
+}
