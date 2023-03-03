f2551370ee0a6d90430170486be59942d03eab23
tuyucheng <tuyucheng2000@163.com>
2023-03-03 10:34:46 +0800

    🚚 Updated test names

--- design-patterns-modules/design-patterns-structural/src/test/java/cn/tuyucheng/taketoday/pipeline/PipeUnitTest.java
+++ design-patterns-modules/design-patterns-structural/src/test/java/cn/tuyucheng/taketoday/pipeline/PipeUnitTest.java
@@ -7,14 +7,14 @@ import org.junit.jupiter.api.Test;
 
 class PipeUnitTest {
 
-    @Test
-    void simplePipeTest() {
-        Pipe<Integer, Integer> square = s -> s * s;
-        Pipe<Integer, Integer> half = s -> s / 2;
-        Pipe<Integer, String> toString = Object::toString;
-        Pipe<Integer, String> pipeline = square.add(half).add(toString);
-        String result = pipeline.process(5);
-        String expected = "12";
-        assertThat(result).isEqualTo(expected);
-    }
+	@Test
+	void whenCombiningThreePipes_andInitializingPipeline_thenResultIsCorrect() {
+		Pipe<Integer, Integer> square = s -> s * s;
+		Pipe<Integer, Integer> half = s -> s / 2;
+		Pipe<Integer, String> toString = Object::toString;
+		Pipe<Integer, String> pipeline = square.add(half).add(toString);
+		String result = pipeline.process(5);
+		String expected = "12";
+		assertThat(result).isEqualTo(expected);
+	}
 }
--- design-patterns-modules/design-patterns-structural/src/test/java/cn/tuyucheng/taketoday/pipeline/PipelineUnitTest.java
+++ design-patterns-modules/design-patterns-structural/src/test/java/cn/tuyucheng/taketoday/pipeline/PipelineUnitTest.java
@@ -8,17 +8,17 @@ import org.junit.jupiter.api.Test;
 
 class PipelineUnitTest {
 
-    @Test
-    void simplePipelineTest() {
-        Pipe<Integer, Integer> square = s -> s * s;
-        Pipe<Integer, Integer> half = s -> s / 2;
-        Pipe<Integer, String> toString = Object::toString;
-        Pipeline<Integer, Integer> squarePipeline = Pipeline.of(square);
-        Pipeline<Integer, Integer> squareAndHalfPipeline = squarePipeline.withNextPipe(half);
-        Pipeline<Integer, String> squareHalfAndStringPipeline = squareAndHalfPipeline.withNextPipe(toString);
+	@Test
+	void whenCombiningThreePipes_andInitializingPipeline_thenResultIsCorrect() {
+		Pipe<Integer, Integer> square = s -> s * s;
+		Pipe<Integer, Integer> half = s -> s / 2;
+		Pipe<Integer, String> toString = Object::toString;
+		Pipeline<Integer, Integer> squarePipeline = Pipeline.of(square);
+		Pipeline<Integer, Integer> squareAndHalfPipeline = squarePipeline.withNextPipe(half);
+		Pipeline<Integer, String> squareHalfAndStringPipeline = squareAndHalfPipeline.withNextPipe(toString);
 
-        String result = squareHalfAndStringPipeline.process(5);
-        String expected = "12";
-        assertThat(result).isEqualTo(expected);
-    }
+		String result = squareHalfAndStringPipeline.process(5);
+		String expected = "12";
+		assertThat(result).isEqualTo(expected);
+	}
 }
