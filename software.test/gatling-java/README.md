c9b698608a5e5bcea42d748464bcace6f73edf45
tuyucheng <tuyucheng2000@163.com>
2023-03-19 10:35:08 +0800

    ✨ Add Java code for Gatling perf test

---
+++ software.test/gatling-java/README.md
@@ -0,0 +1,7 @@
+## 相关文章
+
+使用Gatling负载测试Rest端点
+
+## 运行模拟
+
+从命令提示符运行模拟，请使用mvn gatling:test
---
+++ software.test/gatling-java/src/main/java/cn/tuyucheng/taketoday/Employee.java
@@ -0,0 +1,15 @@
+package cn.tuyucheng.taketoday;
+
+import java.util.Set;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Employee {
+	private String empName;
+	private Address address;
+	private String id;
+	private Set<String> projects;
+}
