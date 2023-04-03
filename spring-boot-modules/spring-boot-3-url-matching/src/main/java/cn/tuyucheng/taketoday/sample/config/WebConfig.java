ad2763a9dcddf848244a484b3b70d9024d06c6dc
tuyucheng <tuyucheng2000@163.com>
2023-04-03 12:24:33 +0800

    ✨ add spring boot 3 url matching code sample

---
+++ spring-boot-modules/spring-boot-3-url-matching/src/main/java/cn/tuyucheng/taketoday/sample/config/WebConfig.java
@@ -0,0 +1,32 @@
+package cn.tuyucheng.taketoday.sample.config;
+
+import cn.tuyucheng.taketoday.sample.filters.TrailingSlashRedirectFilterReactive;
+import jakarta.servlet.Filter;
+import org.springframework.web.server.WebFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import cn.tuyucheng.taketoday.sample.filters.TrailingSlashRedirectFilter;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public WebFilter trailingSlashRedirectReactiveFilter() {
+		return new TrailingSlashRedirectFilterReactive();
+	}
+
+	@Bean
+	public Filter trailingSlashRedirectFilter() {
+		return new TrailingSlashRedirectFilter();
+	}
+
+	@Bean
+	public FilterRegistrationBean<Filter> trailingSlashFilter() {
+		FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(trailingSlashRedirectFilter());
+		registrationBean.addUrlPatterns("/*");
+		return registrationBean;
+	}
+}
