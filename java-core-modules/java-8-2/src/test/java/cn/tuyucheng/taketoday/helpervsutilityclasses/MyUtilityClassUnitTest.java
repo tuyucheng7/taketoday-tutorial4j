97d478cf1a4fd1a8daba516531d17d9ebe175524
tuyucheng <tuyucheng2000@163.com>
2023-04-06 12:15:54 +0800

    ✨ Creating a deep vs shallow copy of an object in Java

---
+++ java-core-modules/java-8-2/src/test/java/cn/tuyucheng/taketoday/helpervsutilityclasses/MyUtilityClassUnitTest.java
@@ -0,0 +1,14 @@
+package cn.tuyucheng.taketoday.helpervsutilityclasses;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyUtilityClassUnitTest {
+
+	@Test
+	void whenUsingUtilityMethods_thenAccessMethodsViaClassName() {
+		assertEquals("INIUBONG", MyUtilityClass.returnUpperCase("iniubong"));
+		assertEquals("accra", MyUtilityClass.returnLowerCase("AcCrA"));
+	}
+}
