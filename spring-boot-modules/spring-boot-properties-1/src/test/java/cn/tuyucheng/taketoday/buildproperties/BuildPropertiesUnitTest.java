f499815ff6abaf2f8a65c5043b573d1414db092b
tuyucheng <tuyucheng2000@163.com>
2023-04-11 22:12:26 +0800

    ✅ Use BuildProperties bean for getting build information

---
+++ spring-boot-modules/spring-boot-properties-1/src/test/java/cn/tuyucheng/taketoday/buildproperties/BuildPropertiesUnitTest.java
@@ -0,0 +1,30 @@
+package cn.tuyucheng.taketoday.buildproperties;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class BuildPropertiesUnitTest {
+	@Autowired
+	private BuildProperties buildProperties;
+
+	@Test
+	void givenBuildPropertiesBean_WhenFetchDefaultBuildProperties_ThenGetValidValues() {
+		Assertions.assertEquals("spring-boot-properties-1", buildProperties.getArtifact());
+		Assertions.assertEquals("cn.tuyucheng.taketoday.spring-boot-modules", buildProperties.getGroup());
+		Assertions.assertEquals("1.0.0", buildProperties.getVersion());
+	}
+
+	@Test
+	void givenBuildPropertiesBean_WhenFetchCustomBuildProprties_ThenGetValidValues() {
+		Assertions.assertEquals("123", buildProperties.get("custom.value"));
+		Assertions.assertNotNull(buildProperties.get("java.version"));
+		Assertions.assertEquals("Spring Boot Properties Module", buildProperties.get("description"));
+	}
+}
