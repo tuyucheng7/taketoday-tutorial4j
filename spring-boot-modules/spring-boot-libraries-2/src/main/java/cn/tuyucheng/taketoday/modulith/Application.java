1ec518a18e60dd6a41655a1c1a9c78097cd78e0f
tuyucheng <tuyucheng2000@163.com>
2023-04-23 11:28:04 +0800

    ✨ Introduction to Spring Modulith

---
+++ spring-boot-modules/spring-boot-libraries-2/src/main/java/cn/tuyucheng/taketoday/modulith/Application.java
@@ -0,0 +1,21 @@
+package cn.tuyucheng.taketoday.modulith;
+
+import cn.tuyucheng.taketoday.modulith.product.ProductService;
+import cn.tuyucheng.taketoday.modulith.product.internal.Product;
+import org.jobrunr.autoconfigure.JobRunrAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {JobRunrAutoConfiguration.class})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args)
+			.getBean(ProductService.class)
+			.create(new Product("tuyucheng", "course", 10));
+	}
+}
