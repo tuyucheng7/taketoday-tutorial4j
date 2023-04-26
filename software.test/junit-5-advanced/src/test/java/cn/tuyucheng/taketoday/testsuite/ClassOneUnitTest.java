143fde4a9441ee5f6065e726afe145b25425ff9d
tuyucheng <tuyucheng2000@163.com>
2023-04-26 10:25:25 +0800

    ✅ Creating a Test Suite with JUnit

---
+++ software.test/junit-5-advanced/src/test/java/cn/tuyucheng/taketoday/testsuite/ClassOneUnitTest.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.testsuite;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ClassOneUnitTest {
+
+	@Test
+	public void whenTrue_thenTrue() {
+		Assertions.assertTrue(true);
+	}
+}
---
+++ software.test/junit-5-advanced/src/test/java/cn/tuyucheng/taketoday/testsuite/subpackage/ClassTwoUnitTest.java
@@ -0,0 +1,11 @@
+package cn.tuyucheng.taketoday.testsuite.subpackage;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ClassTwoUnitTest {
+	@Test
+	public void whenTrue_thenTrue() {
+		Assertions.assertTrue(true);
+	}
+}
