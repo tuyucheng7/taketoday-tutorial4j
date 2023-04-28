5ed5bdec05dc0aec6c83a159d228593498223832
tuyucheng <tuyucheng2000@163.com>
2023-04-29 05:33:40 +0800

    🚚 move docker spring boot related modules to `spring-boot-modules`

--- docker-modules/docker-spring-boot-postgres/src/main/java/cn/tuyucheng/taketoday/docker/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.tuyucheng.taketoday.docker;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
