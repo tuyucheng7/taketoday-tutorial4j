17783e2578b201c86323da28e67c7f285b3fc1b4
tuyucheng <tuyucheng2000@163.com>
2023-05-03 19:10:44 +0800

    ⬆️ Update spring-boot-swagger-keycloak module

--- spring-boot-modules/spring-boot-swagger-keycloak/src/main/java/cn/tuyucheng/taketoday/swaggerkeycloak/TodosController.java
+++ spring-boot-modules/spring-boot-swagger-keycloak/src/main/java/cn/tuyucheng/taketoday/swaggerkeycloak/TodosController.java
@@ -1,8 +1,8 @@
 package cn.tuyucheng.taketoday.swaggerkeycloak;
 
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,21 +19,19 @@ import java.util.Map;
 @RequestMapping("/api/v1/todos")
 public class TodosController {
 
-	private final Map<Long, Todo> todos = new HashMap<>();
+   private final Map<Long, Todo> todos = new HashMap<>();
 
-	@PostConstruct
-	public void initData() {
-		todos.put(1L, new Todo(1L, "Install Keycloak", LocalDate.now().plusDays(14)));
-		todos.put(2L, new Todo(2L, "Configure realm", LocalDate.now().plusDays(21)));
-	}
+   @PostConstruct
+   public void initData() {
+      todos.put(1L, new Todo(1L, "Install Keycloak", LocalDate.now().plusDays(14)));
+      todos.put(2L, new Todo(2L, "Configure realm", LocalDate.now().plusDays(21)));
+   }
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	@ApiOperation("Read all todos")
-	@ApiResponses({
-		@ApiResponse(code = 200, message = "The todos were found and returned.")
-	})
-	@PreAuthorize("hasAuthority('SCOPE_read_access')")
-	public Collection<Todo> readAll() {
-		return todos.values();
-	}
+   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+   @Operation(description = "Read all todos")
+   @ApiResponses({@ApiResponse(responseCode = "200", description = "The todos were found and returned.")})
+   @PreAuthorize("hasAuthority('SCOPE_read_access')")
+   public Collection<Todo> readAll() {
+      return todos.values();
+   }
 }
