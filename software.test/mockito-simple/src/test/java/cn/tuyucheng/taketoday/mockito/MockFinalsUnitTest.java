a75043e71744dd59199e404d3a56bde4945a50cb
tuyucheng <tuyucheng2000@163.com>
2023-05-03 19:13:18 +0800

    ⬆️ Mock Final Classes and Methods with Mockito article; MockMaker improvement

--- software.test/mockito-simple/src/test/java/cn/tuyucheng/taketoday/mockito/MockFinalsUnitTest.java
+++ software.test/mockito-simple/src/test/java/cn/tuyucheng/taketoday/mockito/MockFinalsUnitTest.java
@@ -1,30 +1,42 @@
 package cn.tuyucheng.taketoday.mockito;
 
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.MockMakers;
 
 class MockFinalsUnitTest {
 
-	@Test
-	void whenMockFinalMethodMockWorks() {
-		MyList myList = new MyList();
+   @Test
+   void whenMockFinalMethodMockWorks() {
+      MyList myList = new MyList();
 
-		MyList mock = mock(MyList.class);
-		when(mock.finalMethod()).thenReturn(1);
+      MyList mock = mock(MyList.class);
+      when(mock.finalMethod()).thenReturn(1);
 
-		assertThat(mock.finalMethod()).isNotEqualTo(myList.finalMethod());
-	}
+      assertThat(mock.finalMethod()).isNotEqualTo(myList.finalMethod());
+   }
 
-	@Test
-	public void whenMockFinalClassMockWorks() {
-		FinalList finalList = new FinalList();
+   @Test
+   public void whenMockFinalClassMockWorks() {
+      FinalList finalList = new FinalList();
 
-		FinalList mock = mock(FinalList.class);
-		when(mock.size()).thenReturn(2);
+      FinalList mock = mock(FinalList.class);
+      when(mock.size()).thenReturn(2);
 
-		assertThat(mock.size()).isNotEqualTo(finalList.size());
-	}
+      assertThat(mock.size()).isNotEqualTo(finalList.size());
+   }
+
+   @Test
+   public void whenMockFinalMethodMockWorks_withInlineMockMaker() {
+      MyList myList = new MyList();
+
+      MyList mock = mock(MyList.class, withSettings().mockMaker(MockMakers.INLINE));
+      when(mock.finalMethod()).thenReturn(1);
+
+      assertThat(mock.finalMethod()).isNotEqualTo(myList.finalMethod());
+   }
 }
