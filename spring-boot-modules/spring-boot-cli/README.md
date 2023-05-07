de99447456204f0f65d83f9ebaaf1b4f1f127ceb
tuyucheng <tuyucheng2000@163.com>
2023-05-07 10:44:55 +0800

    ♻️ Encode Passwords With Spring Boot CLI

--- spring-boot-modules/spring-boot-cli/README.md
+++ spring-boot-modules/spring-boot-cli/README.md
@@ -5,3 +5,4 @@
 ## 相关文章
 
 + [Spring Boot CLI介绍](docs/SpringBoot-CLI介绍.md)
++ [使用Spring Boot CLI对密码进行编码](docs/使用SpringBoot-CLI对密码进行编码.md)
--- spring-boot-modules/spring-boot-cli/src/test/java/cn/tuyucheng/taketoday/encoding/PasswordEncodingUnitTest.java
+++ spring-boot-modules/spring-boot-cli/src/test/java/cn/tuyucheng/taketoday/encoding/PasswordEncodingUnitTest.java
@@ -1,10 +1,5 @@
 package cn.tuyucheng.taketoday.encoding;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,34 +11,39 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class PasswordEncodingUnitTest {
-	private final static String userName = "baeldung";
-	private final static String passwordDecoded = "baeldungPassword";
+   private final static String userName = "tuyucheng";
+   private final static String passwordDecoded = "tuyuchengPassword";
 
-	private MockMvc mvc;
+   private MockMvc mvc;
 
-	@Autowired
-	private WebApplicationContext webApplicationContext;
+   @Autowired
+   private WebApplicationContext webApplicationContext;
 
-	@Before
-	public void setup() {
-		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-			.apply(springSecurity())
-			.build();
-	}
+   @Before
+   public void setup() {
+      mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply(springSecurity())
+            .build();
+   }
 
-	@Test
-	public void givenRequestWithWrongPassword_shouldFailWith401() throws Exception {
-		mvc.perform(get("/").with(httpBasic(userName, "wrongPassword")))
-			.andExpect(status().isUnauthorized());
-	}
+   @Test
+   public void givenRequestWithWrongPassword_shouldFailWith401() throws Exception {
+      mvc.perform(get("/").with(httpBasic(userName, "wrongPassword")))
+            .andExpect(status().isUnauthorized());
+   }
 
-	@Test
-	public void givenRequestWithCorrectDecodedPassword_houldSucceedWith200() throws Exception {
-		mvc.perform(get("/").with(httpBasic(userName, passwordDecoded)))
-			.andExpect(status().isOk());
-	}
+   @Test
+   public void givenRequestWithCorrectDecodedPassword_shouldSucceedWith200() throws Exception {
+      mvc.perform(get("/").with(httpBasic(userName, passwordDecoded)))
+            .andExpect(status().isOk());
+   }
 }
