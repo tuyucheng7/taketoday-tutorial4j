7f960660fbdb09cd149ff297daa61b50116195f5
tuyucheng <tuyucheng2000@163.com>
2023-05-09 11:02:51 +0800

    ♻️ Update article "The producer-consumer problem example in Java"

--- java-core-modules/java-concurrency-advanced-4/src/main/java/cn/tuyucheng/taketoday/producerconsumer/Consumer.java
+++ java-core-modules/java-concurrency-advanced-4/src/main/java/cn/tuyucheng/taketoday/producerconsumer/Consumer.java
@@ -1,50 +1,54 @@
 package cn.tuyucheng.taketoday.producerconsumer;
 
+import java.util.logging.Logger;
+
 public class Consumer implements Runnable {
-	private final DataQueue dataQueue;
+   private static final Logger log = Logger.getLogger(Consumer.class.getCanonicalName());
+   private final DataQueue dataQueue;
 
-	public Consumer(DataQueue dataQueue) {
-		this.dataQueue = dataQueue;
-	}
+   public Consumer(DataQueue dataQueue) {
+      this.dataQueue = dataQueue;
+   }
 
-	@Override
-	public void run() {
-		consume();
-	}
+   @Override
+   public void run() {
+      consume();
+   }
 
-	public void consume() {
-		while (dataQueue.runFlag) {
-			synchronized (this) {
-				while (dataQueue.isEmpty() && dataQueue.runFlag) {
-					try {
-						dataQueue.waitOnEmpty();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-						break;
-					}
-				}
-				if (!dataQueue.runFlag) {
-					break;
-				}
-				Message message = dataQueue.remove();
-				dataQueue.notifyAllForFull();
-				useMessage(message);
-			}
-		}
-		System.out.println("Consumer Stopped");
-	}
+   public void consume() {
+      while (dataQueue.runFlag) {
+         synchronized (dataQueue) {
+            while (dataQueue.isEmpty() && dataQueue.runFlag) {
+               try {
+                  dataQueue.waitOnEmpty();
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+                  break;
+               }
+            }
+            if (!dataQueue.runFlag) {
+               break;
+            }
+            Message message = dataQueue.remove();
+            dataQueue.notifyAllForFull();
+            useMessage(message);
+         }
+      }
+      log.info("Consumer Stopped");
+   }
 
-	private void useMessage(Message message) {
-		if (message != null) {
-			System.out.printf("[%s] Consuming Message. Id: %d, Data: %f\n", Thread.currentThread().getName(), message.getId(), message.getData());
+   private void useMessage(Message message) {
+      if (message != null) {
+         log.info(String.format("[%s] Consuming Message. Id: %d, Data: %f%n",
+               Thread.currentThread().getName(), message.getId(), message.getData()));
 
-			// Sleeping on random time to make it realistic
-			ThreadUtil.sleep((long) (message.getData() * 100));
-		}
-	}
+         // Sleeping on random time to make it realistic
+         ThreadUtil.sleep((long) (message.getData() * 100));
+      }
+   }
 
-	public void stop() {
-		dataQueue.runFlag = false;
-		dataQueue.notifyAllForEmpty();
-	}
+   public void stop() {
+      dataQueue.runFlag = false;
+      dataQueue.notifyAllForEmpty();
+   }
 }
