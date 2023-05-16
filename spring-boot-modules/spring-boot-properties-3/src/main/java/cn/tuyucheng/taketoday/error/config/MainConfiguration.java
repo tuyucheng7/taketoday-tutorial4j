db4d8c68aa5571e0820247a4d66cfe77707c10a3
tuyucheng <tuyucheng2000@163.com>
2023-05-16 19:06:12 +0800

    🐛 Fix formatting issues, refactor the profiles word in the properties.

--- spring-boot-modules/spring-boot-properties-3/src/main/java/cn/tuyucheng/taketoday/error/config/MainConfiguration.java
+++ spring-boot-modules/spring-boot-properties-3/src/main/java/cn/tuyucheng/taketoday/error/config/MainConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "profile.profileProperties")
+@ConfigurationProperties(prefix = "customProperties")
 public class MainConfiguration {
    private String name;
 
