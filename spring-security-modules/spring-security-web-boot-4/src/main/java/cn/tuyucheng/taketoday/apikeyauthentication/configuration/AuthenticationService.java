e42dc1585c3cf2c9464084eedd7a080100978b9d
tuyucheng <tuyucheng2000@163.com>
2023-05-18 18:53:26 +0800

    ✨ Securing Spring Boot API with API key and secret

---
+++ spring-security-modules/spring-security-web-boot-4/src/main/java/cn/tuyucheng/taketoday/apikeyauthentication/configuration/AuthenticationService.java
@@ -0,0 +1,21 @@
+package cn.tuyucheng.taketoday.apikeyauthentication.configuration;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class AuthenticationService {
+
+   private static final String AUTH_TOKEN_HEADER_NAME = "X-API-KEY";
+   private static final String AUTH_TOKEN = "Tuyucheng";
+
+   public static Authentication getAuthentication(HttpServletRequest request) {
+      String apiKey = request.getHeader(AUTH_TOKEN_HEADER_NAME);
+      if (apiKey != null && apiKey.equals(AUTH_TOKEN)) {
+         return new ApiKeyAuthentication(apiKey, AuthorityUtils.NO_AUTHORITIES);
+      }
+
+      return null;
+   }
+}
