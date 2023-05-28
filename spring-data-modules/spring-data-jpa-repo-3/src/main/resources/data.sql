3bf4fb7c9a254cb3aa9baf6745ba2974d102eddf
tuyucheng <tuyucheng2000@163.com>
2023-05-28 12:51:02 +0800

    ✨ Difference Between findBy and findOneBy in Spring Data JPA

---
+++ spring-data-modules/spring-data-jpa-repo-3/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO person (id, first_name, last_name)
+VALUES (1, 'Azhrioun', 'Abderrahim');
+INSERT INTO person (id, first_name, last_name)
+VALUES (2, 'Brian', 'Wheeler');
+INSERT INTO person (id, first_name, last_name)
+VALUES (3, 'Stella', 'Anderson');
+INSERT INTO person (id, first_name, last_name)
+VALUES (4, 'Stella', 'Wheeler');
---
+++ spring-data-modules/spring-data-jpa-repo-3/src/test/java/cn/tuyucheng/taketoday/spring/data/jpa/listrepositories/repository/PersonRepositoryIntegrationTest.java
@@ -0,0 +1,57 @@
+package cn.tuyucheng.taketoday.spring.data.jpa.listrepositories.repository;
+
+import cn.tuyucheng.taketoday.spring.data.jpa.listrepositories.entity.Person;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class PersonRepositoryIntegrationTest {
+
+   @Autowired
+   private PersonRepository personRepository;
+
+   @Test
+   void givenFirstName_whenCallingFindByFirstName_ThenReturnOnePerson() {
+      Person person = personRepository.findByFirstName("Azhrioun");
+
+      assertNotNull(person);
+      assertEquals("Abderrahim", person.getLastName());
+   }
+
+   @Test
+   void givenLastName_whenCallingFindByLastName_ThenReturnList() {
+      List<Person> person = personRepository.findByLastName("Wheeler");
+
+      assertEquals(2, person.size());
+   }
+
+   @Test
+   void givenFirstName_whenCallingFindOneByFirstName_ThenReturnOnePerson() {
+      Person person = personRepository.findOneByFirstName("Azhrioun");
+
+      assertNotNull(person);
+      assertEquals("Abderrahim", person.getLastName());
+   }
+
+   @Test
+   void givenLastName_whenCallingFindOneByLastName_ThenReturnList() {
+      List<Person> persons = personRepository.findOneByLastName("Wheeler");
+
+      assertEquals(2, persons.size());
+   }
+
+   @Test
+   void givenFirstName_whenCallingFindOneByFirstName_ThenThrowException() {
+      IncorrectResultSizeDataAccessException exception = assertThrows(IncorrectResultSizeDataAccessException.class, () -> {
+         personRepository.findOneByFirstName("Stella");
+      });
+
+      assertEquals("query did not return a unique result: 2", exception.getMessage());
+   }
+}
