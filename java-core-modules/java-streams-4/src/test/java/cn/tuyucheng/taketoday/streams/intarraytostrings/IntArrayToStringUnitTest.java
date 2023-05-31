fe7c2c1cfa29c21d2c35c3852aef0f28f998486b
tuyucheng <tuyucheng2000@163.com>
2023-05-31 15:31:20 +0800

    ✨ Add example code for integer array to strings using streams tutorial

---
+++ java-core-modules/java-streams-4/src/test/java/cn/tuyucheng/taketoday/streams/intarraytostrings/IntArrayToStringUnitTest.java
@@ -0,0 +1,51 @@
+package cn.tuyucheng.taketoday.streams.intarraytostrings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static cn.tuyucheng.taketoday.streams.intarraytostrings.ArrayConversionUtils.convertToString;
+import static cn.tuyucheng.taketoday.streams.intarraytostrings.ArrayConversionUtils.convertToStringArray;
+import static cn.tuyucheng.taketoday.streams.intarraytostrings.ArrayConversionUtils.convertToStringArrayWithBoxing;
+
+public class IntArrayToStringUnitTest {
+
+   @Test
+   public void whenConvertingIntegers_thenHandleStreamOfIntegers() {
+      Integer[] integerNumbers = {1, 2, 3, 4, 5};
+      String[] expectedOutput = {"1", "2", "3", "4", "5"};
+
+      String[] strings = convertToStringArray(integerNumbers);
+
+      Assert.assertArrayEquals(expectedOutput, strings);
+   }
+
+   @Test
+   public void whenConvertingInts_thenHandleIntStream() {
+      int[] intNumbers = {1, 2, 3, 4, 5};
+      String[] expectedOutput = {"1", "2", "3", "4", "5"};
+
+      String[] strings = convertToStringArray(intNumbers);
+
+      Assert.assertArrayEquals(expectedOutput, strings);
+   }
+
+   @Test
+   public void givenAnIntArray_whenBoxingToInteger_thenHandleStreamOfIntegers() {
+      int[] intNumbers = {1, 2, 3, 4, 5};
+      String[] expectedOutput = {"1", "2", "3", "4", "5"};
+
+      String[] strings = convertToStringArrayWithBoxing(intNumbers);
+
+      Assert.assertArrayEquals(expectedOutput, strings);
+   }
+
+   @Test
+   public void givenAnIntArray_whenUsingCollectorsJoining_thenReturnCommaSeparatedString() {
+      int[] intNumbers = {1, 2, 3, 4, 5};
+      String expectedOutput = "1, 2, 3, 4, 5";
+
+      String string = convertToString(intNumbers);
+
+      Assert.assertEquals(expectedOutput, string);
+   }
+}
