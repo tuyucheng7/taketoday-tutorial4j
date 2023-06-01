9e3a4bedf8dce72f32e97cdadd681d8350e13591
tuyucheng <tuyucheng2000@163.com>
2023-06-01 11:16:44 +0800

    🚚 move Gray Box Testing to testing-techniques module

--- software.test/testing-techniques/src/test/java/cn/tuyucheng/taketoday/greyboxtesting/SalaryCommissionPercentageCalculatorUnitTest.java
+++ software.test/testing-techniques/src/test/java/cn/tuyucheng/taketoday/greyboxtesting/SalaryCommissionPercentageCalculatorUnitTest.java
@@ -26,31 +26,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SalaryCommissionPercentageCalculatorUnitTest {
 
-	private final SalaryCommissionPercentageCalculator testTarget = new SalaryCommissionPercentageCalculator();
+   private final SalaryCommissionPercentageCalculator testTarget = new SalaryCommissionPercentageCalculator();
 
-	@ParameterizedTest
-	@MethodSource("provideReferenceTestScenarioTable")
-	void givenReferenceTable_whenCalculateAverageCommission_thenReturnExpectedResult(SalaryCommissionPercentageCalculator.Level level,
-																					 Type type,
-																					 Seniority seniority,
-																					 SalesImpact impact,
-																					 double expected) {
-		BigDecimal got = testTarget.calculate(level, type, seniority, impact);
+   @ParameterizedTest
+   @MethodSource("provideReferenceTestScenarioTable")
+   void givenReferenceTable_whenCalculateAverageCommission_thenReturnExpectedResult(SalaryCommissionPercentageCalculator.Level level,
+                                                                                    Type type,
+                                                                                    Seniority seniority,
+                                                                                    SalesImpact impact,
+                                                                                    double expected) {
+      BigDecimal got = testTarget.calculate(level, type, seniority, impact);
 
-		assertEquals(BigDecimal.valueOf(expected), got);
-	}
+      assertEquals(BigDecimal.valueOf(expected), got);
+   }
 
-	private static Stream<Arguments> provideReferenceTestScenarioTable() {
-		return Stream.of(
-			Arguments.of(L1, FULL_TIME_COMMISSIONED, JR, LOW, 0.26),
-			Arguments.of(L1, CONTRACTOR, SR, MEDIUM, 0.12),
-			Arguments.of(L1, FREELANCER, MID, HIGH, 0.11),
-			Arguments.of(L2, FULL_TIME_COMMISSIONED, SR, HIGH, 0.18),
-			Arguments.of(L2, CONTRACTOR, MID, LOW, 0.11),
-			Arguments.of(L2, FREELANCER, JR, MEDIUM, 0.24),
-			Arguments.of(L3, FULL_TIME_COMMISSIONED, MID, MEDIUM, 0.17),
-			Arguments.of(L3, CONTRACTOR, JR, HIGH, 0.28),
-			Arguments.of(L3, FREELANCER, SR, LOW, 0.12)
-		);
-	}
+   private static Stream<Arguments> provideReferenceTestScenarioTable() {
+      return Stream.of(
+            Arguments.of(L1, FULL_TIME_COMMISSIONED, JR, LOW, 0.26),
+            Arguments.of(L1, CONTRACTOR, SR, MEDIUM, 0.12),
+            Arguments.of(L1, FREELANCER, MID, HIGH, 0.11),
+            Arguments.of(L2, FULL_TIME_COMMISSIONED, SR, HIGH, 0.18),
+            Arguments.of(L2, CONTRACTOR, MID, LOW, 0.11),
+            Arguments.of(L2, FREELANCER, JR, MEDIUM, 0.24),
+            Arguments.of(L3, FULL_TIME_COMMISSIONED, MID, MEDIUM, 0.17),
+            Arguments.of(L3, CONTRACTOR, JR, HIGH, 0.28),
+            Arguments.of(L3, FREELANCER, SR, LOW, 0.12)
+      );
+   }
 }
