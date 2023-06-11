fa5f03417a40fc08fa377d2cedfb0821d11f3bfa
tuyucheng <tuyucheng2000@163.com>
2023-06-11 21:54:55 +0800

    🐛 Fix the integration tests which failed due to defined port

--- spring-security-modules/spring-security-web-boot-5/src/test/java/cn/tuyucheng/taketoday/customauth/controller/ApiControllerIntegrationTest.java
+++ spring-security-modules/spring-security-web-boot-5/src/test/java/cn/tuyucheng/taketoday/customauth/controller/ApiControllerIntegrationTest.java
@@ -5,31 +5,31 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = SpringSecurityApplication.class,
-      webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = SpringSecurityApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 class ApiControllerIntegrationTest {
 
-   private static final String API_ENDPOINT = "http://localhost:8080/app/api/hello";
    private final TestRestTemplate restTemplate = new TestRestTemplate();
+
+   private static final String API_ENDPOINT = "http://localhost:%s/app/api/hello";
+
+   @LocalServerPort
+   private int serverPort;
 
    @Test
    void givenAuthHeaderSecretIsValid_whenApiControllerCalled_thenReturnOk() throws Exception {
       HttpHeaders headers = new HttpHeaders();
       headers.add("x-auth-secret-key", "test-secret");
 
-      ResponseEntity<String> response = restTemplate.exchange(new URI(API_ENDPOINT), HttpMethod.GET,
+      ResponseEntity<String> response = restTemplate.exchange(new URI(String.format(API_ENDPOINT, serverPort)), HttpMethod.GET,
             new HttpEntity<>(headers), String.class);
 
       assertEquals(HttpStatus.OK, response.getStatusCode());
@@ -41,7 +41,7 @@ class ApiControllerIntegrationTest {
       HttpHeaders headers = new HttpHeaders();
       headers.add("x-auth-secret-key", "invalid");
 
-      ResponseEntity<String> response = restTemplate.exchange(new URI(API_ENDPOINT), HttpMethod.GET,
+      ResponseEntity<String> response = restTemplate.exchange(new URI(String.format(API_ENDPOINT, serverPort)), HttpMethod.GET,
             new HttpEntity<>(headers), String.class);
 
       assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
@@ -52,7 +52,7 @@ class ApiControllerIntegrationTest {
       HttpHeaders headers = new HttpHeaders();
       headers.add("x-auth-secret", "test-secret");
 
-      ResponseEntity<String> response = restTemplate.exchange(new URI(API_ENDPOINT), HttpMethod.GET,
+      ResponseEntity<String> response = restTemplate.exchange(new URI(String.format(API_ENDPOINT, serverPort)), HttpMethod.GET,
             new HttpEntity<>(headers), String.class);
 
       assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
@@ -62,7 +62,7 @@ class ApiControllerIntegrationTest {
    void givenAuthHeaderIsMissing_whenApiControllerCalled_thenReturnUnAuthorised() throws Exception {
       HttpHeaders headers = new HttpHeaders();
 
-      ResponseEntity<String> response = restTemplate.exchange(new URI(API_ENDPOINT), HttpMethod.GET,
+      ResponseEntity<String> response = restTemplate.exchange(new URI(String.format(API_ENDPOINT, serverPort)), HttpMethod.GET,
             new HttpEntity<>(headers), String.class);
 
       assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
--- spring-security-modules/spring-security-web-boot-5/src/test/java/cn/tuyucheng/taketoday/customauth/controller/HealthCheckControllerIntegrationTest.java
+++ spring-security-modules/spring-security-web-boot-5/src/test/java/cn/tuyucheng/taketoday/customauth/controller/HealthCheckControllerIntegrationTest.java
@@ -5,30 +5,30 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest(classes = SpringSecurityApplication.class,
-      webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = SpringSecurityApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(SpringExtension.class)
 class HealthCheckControllerIntegrationTest {
 
-   private static final String HEALTH_CHECK_ENDPOINT = "http://localhost:8080/app/health";
    private final TestRestTemplate restTemplate = new TestRestTemplate();
+
+   private static final String HEALTH_CHECK_ENDPOINT = "http://localhost:%s/app/health";
+
+   @LocalServerPort
+   private int serverPort;
 
    @Test
    void givenApplicationIsRunning_whenHealthCheckControllerCalled_thenReturnOk() throws Exception {
       HttpHeaders headers = new HttpHeaders();
 
-      ResponseEntity<String> response = restTemplate.exchange(new URI(HEALTH_CHECK_ENDPOINT), HttpMethod.GET,
+      ResponseEntity<String> response = restTemplate.exchange(new URI(String.format(HEALTH_CHECK_ENDPOINT, serverPort)), HttpMethod.GET,
             new HttpEntity<>(headers), String.class);
 
       assertEquals(HttpStatus.OK, response.getStatusCode());
