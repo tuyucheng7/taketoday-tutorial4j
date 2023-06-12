f03788527cb5937963ff6d4752b72c313bcf2767
tuyucheng <tuyucheng2000@163.com>
2023-06-12 12:55:17 +0800

    🐛 fix testcase

--- java-core-modules/java-jvm-1/src/test/java/cn/tuyucheng/taketoday/classloader/CustomClassLoaderUnitTest.java
+++ java-core-modules/java-jvm-1/src/test/java/cn/tuyucheng/taketoday/classloader/CustomClassLoaderUnitTest.java
@@ -1,6 +1,5 @@
 package cn.tuyucheng.taketoday.classloader;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.lang.reflect.InvocationTargetException;
@@ -8,15 +7,14 @@ import java.lang.reflect.Method;
 
 public class CustomClassLoaderUnitTest {
 
-	@Test
-	@Ignore
-	public void customLoader() throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-		CustomClassLoader customClassLoader = new CustomClassLoader();
-		Class<?> c = customClassLoader.findClass(PrintClassLoader.class.getName());
+   @Test
+   public void customLoader() throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+      CustomClassLoader customClassLoader = new CustomClassLoader();
+      Class<?> c = customClassLoader.findClass(PrintClassLoader.class.getName());
 
-		Object ob = c.newInstance();
+      Object ob = c.newInstance();
 
-		Method md = c.getMethod("printClassLoaders");
-		md.invoke(ob);
-	}
+      Method md = c.getMethod("printClassLoaders");
+      md.invoke(ob);
+   }
 }
