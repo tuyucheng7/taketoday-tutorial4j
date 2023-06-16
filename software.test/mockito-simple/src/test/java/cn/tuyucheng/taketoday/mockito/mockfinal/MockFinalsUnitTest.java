abcb5371fd9fdc802f0e55d260fa45cd0f1e7665
tuyucheng <tuyucheng2000@163.com>
2023-06-16 14:44:30 +0800

    ♻️ Removed mockmaker file, not needed with mockito v5+

--- software.test/mockito-simple/src/test/java/cn/tuyucheng/taketoday/mockito/mockfinal/MockFinalsUnitTest.java
+++ software.test/mockito-simple/src/test/java/cn/tuyucheng/taketoday/mockito/mockfinal/MockFinalsUnitTest.java
@@ -1,14 +1,13 @@
 package cn.tuyucheng.taketoday.mockito.mockfinal;
 
-import cn.tuyucheng.taketoday.mockito.FinalList;
-import cn.tuyucheng.taketoday.mockito.MyList;
-import org.junit.jupiter.api.Test;
-import org.mockito.MockMakers;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.withSettings;
+
+import org.junit.jupiter.api.Test;
+
+import cn.tuyucheng.taketoday.mockito.FinalList;
+import cn.tuyucheng.taketoday.mockito.MyList;
 
 class MockFinalsUnitTest {
 
@@ -28,15 +27,5 @@ class MockFinalsUnitTest {
       when(mock.size()).thenReturn(2);
 
       assertThat(mock.size()).isNotEqualTo(1);
-   }
-
-   @Test
-   public void whenMockFinalMethodMockWorks_withInlineMockMaker() {
-      MyList myList = new MyList();
-
-      MyList mock = mock(MyList.class, withSettings().mockMaker(MockMakers.INLINE));
-      when(mock.finalMethod()).thenReturn(1);
-
-      assertThat(mock.finalMethod()).isNotEqualTo(myList.finalMethod());
    }
 }
