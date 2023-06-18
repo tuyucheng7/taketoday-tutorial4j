86f16d64ebe9b95fd3e962dfd4275a7cacc00c9f
tuyucheng <tuyucheng2000@163.com>
2023-06-18 17:29:31 +0800

    🐛 Fix test

--- spring-data-modules/spring-data-dynamodb/src/test/java/cn/tuyucheng/taketoday/spring/data/dynamodb/repository/ProductInfoRepositoryIntegrationTest.java
+++ spring-data-modules/spring-data-dynamodb/src/test/java/cn/tuyucheng/taketoday/spring/data/dynamodb/repository/ProductInfoRepositoryIntegrationTest.java
@@ -1,26 +1,35 @@
 package cn.tuyucheng.taketoday.spring.data.dynamodb.repository;
 
-import cn.tuyucheng.taketoday.Application;
-import cn.tuyucheng.taketoday.spring.data.dynamodb.model.ProductInfo;
-import cn.tuyucheng.taketoday.spring.data.dynamodb.repositories.ProductInfoRepository;
-import cn.tuyucheng.taketoday.spring.data.dynamodb.repository.rule.LocalDbCreationRule;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
 import com.amazonaws.services.dynamodbv2.model.ResourceInUseException;
+import cn.tuyucheng.taketoday.Application;
+import cn.tuyucheng.taketoday.spring.data.dynamodb.model.ProductInfo;
+import cn.tuyucheng.taketoday.spring.data.dynamodb.repositories.ProductInfoRepository;
+import cn.tuyucheng.taketoday.spring.data.dynamodb.repository.rule.LocalDbCreationRule;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
 
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
@@ -31,48 +40,90 @@ import static org.junit.Assert.assertThat;
 @SpringBootTest(classes = Application.class)
 @WebAppConfiguration
 @ActiveProfiles("local")
-@TestPropertySource(properties = {"amazon.dynamodb.endpoint=http://localhost:8000/", "amazon.aws.accesskey=test1", "amazon.aws.secretkey=test231"})
 public class ProductInfoRepositoryIntegrationTest {
 
-    @ClassRule
-    public static LocalDbCreationRule dynamoDB = new LocalDbCreationRule();
+   @ClassRule
+   public static LocalDbCreationRule dynamoDB = new LocalDbCreationRule();
+   private static DynamoDBMapper dynamoDBMapper;
+   private static AmazonDynamoDB amazonDynamoDB;
 
-    private DynamoDBMapper dynamoDBMapper;
+   @Autowired
+   private ProductInfoRepository repository;
 
-    @Autowired
-    private AmazonDynamoDB amazonDynamoDB;
+   private static final String DYNAMODB_ENDPOINT = "amazon.dynamodb.endpoint";
+   private static final String AWS_ACCESSKEY = "amazon.aws.accesskey";
+   private static final String AWS_SECRETKEY = "amazon.aws.secretkey";
 
-    @Autowired
-    ProductInfoRepository repository;
+   private static final String EXPECTED_COST = "20";
+   private static final String EXPECTED_PRICE = "50";
 
-    private static final String EXPECTED_COST = "20";
-    private static final String EXPECTED_PRICE = "50";
+   @BeforeClass
+   public static void setupClass() {
+      Properties testProperties = loadFromFileInClasspath("application.properties")
+            .filter(properties -> !isEmpty(properties.getProperty(AWS_ACCESSKEY)))
+            .filter(properties -> !isEmpty(properties.getProperty(AWS_SECRETKEY)))
+            .filter(properties -> !isEmpty(properties.getProperty(DYNAMODB_ENDPOINT)))
+            .orElseThrow(() -> new RuntimeException("Unable to get all of the required test property values"));
 
-    @Before
-    public void setup() throws Exception {
-        try {
-            dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+      String amazonAWSAccessKey = testProperties.getProperty(AWS_ACCESSKEY);
+      String amazonAWSSecretKey = testProperties.getProperty(AWS_SECRETKEY);
+      String amazonDynamoDBEndpoint = testProperties.getProperty(DYNAMODB_ENDPOINT);
 
-            CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(ProductInfo.class);
+      amazonDynamoDB = new AmazonDynamoDBClient(new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey));
+      amazonDynamoDB.setEndpoint(amazonDynamoDBEndpoint);
+      dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+   }
 
-            tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+   @Before
+   public void setup() throws Exception {
+      try {
+         dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
 
-            amazonDynamoDB.createTable(tableRequest);
-        } catch (ResourceInUseException e) {
-            // Do nothing, table already created
-        }
+         CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(ProductInfo.class);
 
-        // TODO How to handle different environments. i.e. AVOID deleting all entries in ProductInfo on table
-        dynamoDBMapper.batchDelete((List<ProductInfo>) repository.findAll());
-    }
+         tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
 
-    @Test
-    public void givenItemWithExpectedCost_whenRunFindAll_thenItemIsFound() {
-        ProductInfo productInfo = new ProductInfo(EXPECTED_COST, EXPECTED_PRICE);
-        repository.save(productInfo);
+         amazonDynamoDB.createTable(tableRequest);
+      } catch (ResourceInUseException e) {
+         // Do nothing, table already created
+      }
 
-        List<ProductInfo> result = (List<ProductInfo>) repository.findAll();
-        assertThat(result.size(), is(greaterThan(0)));
-        assertThat(result.get(0).getCost(), is(equalTo(EXPECTED_COST)));
-    }
+      // TODO How to handle different environments. i.e. AVOID deleting all entries in ProductInfo on table
+      dynamoDBMapper.batchDelete((List<ProductInfo>) repository.findAll());
+   }
+
+   @Test
+   public void givenItemWithExpectedCost_whenRunFindAll_thenItemIsFound() {
+
+      ProductInfo productInfo = new ProductInfo(EXPECTED_COST, EXPECTED_PRICE);
+      repository.save(productInfo);
+
+      List<ProductInfo> result = (List<ProductInfo>) repository.findAll();
+      assertThat(result.size(), is(greaterThan(0)));
+      assertThat(result.get(0).getCost(), is(equalTo(EXPECTED_COST)));
+   }
+
+   private static boolean isEmpty(String inputString) {
+      return inputString == null || "".equals(inputString);
+   }
+
+   private static Optional<Properties> loadFromFileInClasspath(String fileName) {
+      InputStream stream = null;
+      try {
+         Properties config = new Properties();
+         Path configLocation = Paths.get(ClassLoader.getSystemResource(fileName).toURI());
+         stream = Files.newInputStream(configLocation);
+         config.load(stream);
+         return Optional.of(config);
+      } catch (Exception e) {
+         return Optional.empty();
+      } finally {
+         if (stream != null) {
+            try {
+               stream.close();
+            } catch (IOException e) {
+            }
+         }
+      }
+   }
 }
