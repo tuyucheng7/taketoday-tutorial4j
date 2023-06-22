0179907a2079ee994f174cf2b0949b183189b088
tuyucheng <tuyucheng2000@163.com>
2023-06-22 20:10:06 +0800

    ⬆️ upgraded microprofile version

--- microservices/microprofile/src/main/java/cn/tuyucheng/taketoday/microprofile/providers/BookMessageBodyWriter.java
+++ microservices/microprofile/src/main/java/cn/tuyucheng/taketoday/microprofile/providers/BookMessageBodyWriter.java
@@ -2,16 +2,16 @@ package cn.tuyucheng.taketoday.microprofile.providers;
 
 import cn.tuyucheng.taketoday.microprofile.model.Book;
 import cn.tuyucheng.taketoday.microprofile.util.BookMapper;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonWriter;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.MessageBodyWriter;
+import jakarta.ws.rs.ext.Provider;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonWriter;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyWriter;
-import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
@@ -20,37 +20,39 @@ import java.lang.reflect.Type;
 @Provider
 @Produces(MediaType.APPLICATION_JSON)
 public class BookMessageBodyWriter implements MessageBodyWriter<Book> {
-    @Override
-    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-        return type.equals(Book.class);
-    }
 
-    /*
-    Deprecated in JAX RS 2.0
-     */
-    @Override
-    public long getSize(Book book, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
-        return 0;
-    }
+   @Override
+   public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+      return type.equals(Book.class);
+   }
 
-    /**
-     * Marsahl Book to OutputStream
-     *
-     * @param book
-     * @param type
-     * @param genericType
-     * @param annotations
-     * @param mediaType
-     * @param httpHeaders
-     * @param entityStream
-     * @throws IOException
-     * @throws WebApplicationException
-     */
-    @Override
-    public void writeTo(Book book, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
-        JsonWriter jsonWriter = Json.createWriter(entityStream);
-        JsonObject jsonObject = BookMapper.map(book);
-        jsonWriter.writeObject(jsonObject);
-        jsonWriter.close();
-    }
+   /*
+      Deprecated in JAX RS 2.0
+    */
+   @Override
+   public long getSize(Book book, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+      return 0;
+   }
+
+   /**
+    * Marsahl Book to OutputStream
+    *
+    * @param book
+    * @param type
+    * @param genericType
+    * @param annotations
+    * @param mediaType
+    * @param httpHeaders
+    * @param entityStream
+    * @throws IOException
+    * @throws WebApplicationException
+    */
+   @Override
+   public void writeTo(Book book, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                       MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+      JsonWriter jsonWriter = Json.createWriter(entityStream);
+      JsonObject jsonObject = BookMapper.map(book);
+      jsonWriter.writeObject(jsonObject);
+      jsonWriter.close();
+   }
 }
