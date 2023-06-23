ff7e3866031451e8531a72e30c1e863cb7d64954
tuyucheng <tuyucheng2000@163.com>
2023-06-23 17:34:38 +0800

    🔥 remove postgres-notify

--- messaging-modules/postgres-notify/src/main/java/cn/tuyucheng/taketoday/messaging/postgresql/repository/OrdersRepository.java
+++
@@ -1,8 +0,0 @@
-package cn.tuyucheng.taketoday.messaging.postgresql.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import cn.tuyucheng.taketoday.messaging.postgresql.domain.Order;
-
-public interface OrdersRepository extends CrudRepository<Order, Long> {
-}
