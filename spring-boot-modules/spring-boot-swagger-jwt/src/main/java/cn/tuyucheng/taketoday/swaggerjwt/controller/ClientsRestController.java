81dd66d909668cedf1323ec16e9408369a8e2475
tuyucheng <tuyucheng2000@163.com>
2023-06-24 13:23:16 +0800

    ⬆️ Upgrade spring-boot-swagger-jwt modules to use SpringDoc in place of SpringFox

--- spring-boot-modules/spring-boot-swagger-jwt/src/main/java/cn/tuyucheng/taketoday/swaggerjwt/controller/ClientsRestController.java
+++ spring-boot-modules/spring-boot-swagger-jwt/src/main/java/cn/tuyucheng/taketoday/swaggerjwt/controller/ClientsRestController.java
@@ -1,7 +1,7 @@
 package cn.tuyucheng.taketoday.swaggerjwt.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +9,12 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController(value = "/clients")
-@Api(tags = "Clients")
+@Tag(name = "Clients")
 public class ClientsRestController {
 
-	@ApiOperation(value = "This method is used to get the clients.")
-	@GetMapping
-	public List<String> getClients() {
-		return Arrays.asList("First Client", "Second Client");
-	}
+    @Operation(summary = "This method is used to get the clients.")
+    @GetMapping
+    public List<String> getClients() {
+        return Arrays.asList("First Client", "Second Client");
+    }
 }
