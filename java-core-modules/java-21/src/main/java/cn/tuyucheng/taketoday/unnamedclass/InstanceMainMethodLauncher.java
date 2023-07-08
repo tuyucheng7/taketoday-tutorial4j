cbbffa2879b2a2b79b06f715a01491ba53a8feae
tuyucheng <tuyucheng2000@163.com>
2023-07-09 01:43:49 +0800

    ✨ JEP 445(Unnamed Classes and Instance Main Methods) examples

---
+++ java-core-modules/java-21/src/main/java/cn/tuyucheng/taketoday/unnamedclass/InstanceMainMethodLauncher.java
@@ -0,0 +1,10 @@
+package cn.tuyucheng.taketoday.unnamedclass;
+
+/**
+ * To run: `java --enable-preview --source 21 InstanceMainMethodLauncher.java`
+ */
+class InstanceMainMethodLauncher {
+   void main() {
+      System.out.println("Hello world from an instance main method o/");
+   }
+}
