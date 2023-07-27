51d257a85c825edda846a71ea43712471b25fe5f
taketoday <tuyucheng2000@163.com>
2023-07-27 20:11:11 +0800

    ✨ Added spring data reactive pagination along with tests

---
+++ spring-reactive-modules/spring-5-data-reactive-2/README.md
@@ -0,0 +1,7 @@
+## Spring Data Reactive Project
+
+本模块包含有关Spring 5 Data Reactive的文章
+
+## 相关文章
+
++ []()
---
+++ spring-reactive-modules/spring-5-data-reactive-2/src/main/java/cn/tuyucheng/taketoday/pagination/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package cn.tuyucheng.taketoday.pagination.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import cn.tuyucheng.taketoday.pagination.model.Product;
+
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveSortingRepository<Product, UUID> {
+
+   Flux<Product> findAllBy(Pageable pageable);
+}
