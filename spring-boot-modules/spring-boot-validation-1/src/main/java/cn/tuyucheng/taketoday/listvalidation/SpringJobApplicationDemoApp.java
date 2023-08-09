e4eaca285d5bcf3ec433e7293719e1b821b0e8e1
taketoday <tuyucheng2000@163.com>
2023-08-09 19:49:59 +0800

    ✨ Validate string length with Java validation annotations

---
+++ spring-boot-modules/spring-boot-validation-1/src/main/java/cn/tuyucheng/taketoday/listvalidation/SpringJobApplicationDemoApp.java
@@ -0,0 +1,11 @@
+package cn.tuyucheng.taketoday.listvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobApplicationDemoApp {
+   public static void main(String[] args) {
+      SpringApplication.run(SpringJobApplicationDemoApp.class, args);
+   }
+}
