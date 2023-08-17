9e996d8ffc78bcd26082970de56ee3e9e1021849
taketoday <tuyucheng2000@163.com>
2023-08-17 20:54:32 +0800

    🐛 Potential issue in A Quick Guide to Using Keycloak With Spring Boot article

--- spring-boot-modules/spring-boot-keycloak-1/src/main/java/cn/tuyucheng/taketoday/keycloak/SecurityConfig.java
+++ spring-boot-modules/spring-boot-keycloak-1/src/main/java/cn/tuyucheng/taketoday/keycloak/SecurityConfig.java
@@ -2,6 +2,7 @@ package cn.tuyucheng.taketoday.keycloak;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,36 +17,48 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @EnableWebSecurity
 class SecurityConfig {
 
-	private final KeycloakLogoutHandler keycloakLogoutHandler;
+   private final KeycloakLogoutHandler keycloakLogoutHandler;
 
-	SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-		this.keycloakLogoutHandler = keycloakLogoutHandler;
-	}
+   SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+      this.keycloakLogoutHandler = keycloakLogoutHandler;
+   }
 
-	@Bean
-	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-	}
+   @Bean
+   protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+      return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+   }
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/customers*")
-			.hasRole("USER")
-			.anyRequest()
-			.permitAll();
-		http.oauth2Login()
-			.and()
-			.logout()
-			.addLogoutHandler(keycloakLogoutHandler)
-			.logoutSuccessUrl("/");
-		http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-		return http.build();
-	}
+   @Order(1)
+   @Bean
+   public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+            .antMatchers("/")
+            .permitAll()
+            .anyRequest()
+            .authenticated();
+      http.oauth2Login()
+            .and()
+            .logout()
+            .addLogoutHandler(keycloakLogoutHandler)
+            .logoutSuccessUrl("/");
+      return http.build();
+   }
 
-	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-		return http.getSharedObject(AuthenticationManagerBuilder.class)
-			.build();
-	}
+   @Order(2)
+   @Bean
+   public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+            .antMatchers("/customers*")
+            .hasRole("USER")
+            .anyRequest()
+            .authenticated();
+      http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+      return http.build();
+   }
+
+   @Bean
+   public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+      return http.getSharedObject(AuthenticationManagerBuilder.class)
+            .build();
+   }
 }
