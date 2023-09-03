24cdae42b3e1d30467aac87a76ef3e2505409815
taketoday <tuyucheng2000@163.com>
2023-09-03 12:11:16 +0800

    ✨ Inner Classes vs. Subclasses in Java

---
+++ java-core-modules/java-lang-oop-inheritance/src/test/java/cn/tuyucheng/taketoday/subclassinnerclass/InnerClassUnitTest.java
@@ -0,0 +1,17 @@
+package cn.tuyucheng.taketoday.subclassinnerclass;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class InnerClassUnitTest {
+
+   @Test
+   public void givenInnerStaticClassWhenInstantiatedThenOuterClassIsInstantiated() {
+      Notifier emailNotifier = new EmailNotifier();
+      EmailNotifier.EmailConnector emailConnector = new EmailNotifier.EmailConnector();
+
+      assertThat(emailNotifier).hasSameClassAs(new EmailNotifier());
+      assertThat(emailConnector).isInstanceOf(EmailNotifier.EmailConnector.class);
+   }
+}
