9a09c149db037f93fe9c9bf3fe75bc4655f6f827
taketoday <tuyucheng2000@163.com>
2023-09-09 14:20:01 +0800

    ✨ initial commit

---
+++ software.test/mockito-2/src/main/java/cn/tuyucheng/taketoday/constructor/UserService.java
@@ -0,0 +1,18 @@
+package cn.tuyucheng.taketoday.constructor;
+
+public class UserService {
+
+   private final String userName;
+
+   public UserService(String userName) {
+      this.userName = userName;
+   }
+
+   public UserService() {
+      this.userName = "Unknown";
+   }
+
+   public String getUserName() {
+      return this.userName;
+   }
+}
