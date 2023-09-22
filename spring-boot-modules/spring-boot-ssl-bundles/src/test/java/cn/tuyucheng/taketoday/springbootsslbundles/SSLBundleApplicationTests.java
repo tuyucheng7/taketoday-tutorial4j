e5f1dfb7f67b9986f261b7eaf8492555f79ae3f5
taketoday <tuyucheng2000@163.com>
2023-09-22 20:18:10 +0800

    ✨ Securing Spring Boot 3 Applications with SSL Bundles

---
+++ spring-boot-modules/spring-boot-ssl-bundles/src/test/java/cn/tuyucheng/taketoday/springbootsslbundles/SSLBundleApplicationTests.java
@@ -0,0 +1,12 @@
+package cn.tuyucheng.taketoday.springbootsslbundles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSLBundleApplicationTests {
+   @Test
+   void contextLoads() {
+
+   }
+}
