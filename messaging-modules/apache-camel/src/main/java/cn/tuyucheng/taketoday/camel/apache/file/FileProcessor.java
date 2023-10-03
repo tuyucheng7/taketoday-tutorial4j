e8012b9b7c7c79087254e4d0d72e53a10f9fe7cc
taketoday <tuyucheng2000@163.com>
2023-10-03 19:11:32 +0800

    ✨ add Spring Bean Annotation

--- messaging-modules/apache-camel/src/main/java/cn/tuyucheng/taketoday/camel/apache/file/FileProcessor.java
+++ messaging-modules/apache-camel/src/main/java/cn/tuyucheng/taketoday/camel/apache/file/FileProcessor.java
@@ -2,19 +2,20 @@ package cn.tuyucheng.taketoday.camel.apache.file;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@Component
 public class FileProcessor implements Processor {
 
-    public void process(Exchange exchange) throws Exception {
-        String originalFileName = (String) exchange.getIn().getHeader(Exchange.FILE_NAME, String.class);
+   public void process(Exchange exchange) throws Exception {
+      String originalFileName = (String) exchange.getIn().getHeader(Exchange.FILE_NAME, String.class);
 
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
-        String changedFileName = dateFormat.format(date) + originalFileName;
-        exchange.getIn().setHeader(Exchange.FILE_NAME, changedFileName);
-    }
-
+      Date date = new Date();
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+      String changedFileName = dateFormat.format(date) + originalFileName;
+      exchange.getIn().setHeader(Exchange.FILE_NAME, changedFileName);
+   }
 }
