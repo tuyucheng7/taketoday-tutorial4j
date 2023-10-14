28b1e6a45fb4609be09c5b8b268596944c51f365
taketoday <tuyucheng2000@163.com>
2023-10-14 12:21:44 +0800

    ♻️ migrate to junit5, added small changes

--- spring-data-modules/spring-data-jpa-enterprise-1/src/test/java/cn/tuyucheng/taketoday/boot/daos/UserRepositoryTCAutoLiveTest.java
+++ spring-data-modules/spring-data-jpa-enterprise-1/src/test/java/cn/tuyucheng/taketoday/boot/daos/UserRepositoryTCAutoLiveTest.java
@@ -2,15 +2,16 @@ package cn.tuyucheng.taketoday.boot.daos;
 
 import cn.tuyucheng.taketoday.boot.Application;
 import cn.tuyucheng.taketoday.boot.domain.User;
-import cn.tuyucheng.taketoday.util.TuyuchengPostgresqlContainer;
-import org.junit.ClassRule;
+import cn.tuyucheng.taketoday.util.BaeldungPostgresqlContainer;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
 
@@ -19,25 +20,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Created by adam.
  */
-@ExtendWith(SpringExtension.class)
+@Testcontainers
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles({"tc", "tc-auto"})
-class UserRepositoryTCAutoLiveTest extends UserRepositoryCommon {
+public class UserRepositoryTCAutoLiveTest extends UserRepositoryCommon {
 
-    @ClassRule
-    public static PostgreSQLContainer<TuyuchengPostgresqlContainer> postgreSQLContainer = TuyuchengPostgresqlContainer.getInstance();
+   @Container
+   public static PostgreSQLContainer<BaeldungPostgresqlContainer> postgreSQLContainer = BaeldungPostgresqlContainer.getInstance();
 
-    @Test
-    @Transactional
-    void givenUsersInDB_WhenUpdateStatusForNameModifyingQueryAnnotationNativePostgres_ThenModifyMatchingUsers() {
-        userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL, ACTIVE_STATUS));
-        userRepository.save(new User("SAMPLE1", LocalDate.now(), USER_EMAIL2, ACTIVE_STATUS));
-        userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL3, ACTIVE_STATUS));
-        userRepository.save(new User("SAMPLE3", LocalDate.now(), USER_EMAIL4, ACTIVE_STATUS));
-        userRepository.flush();
+   @Test
+   @Transactional
+   public void givenUsersInDB_WhenUpdateStatusForNameModifyingQueryAnnotationNativePostgres_ThenModifyMatchingUsers() {
+      userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL, ACTIVE_STATUS));
+      userRepository.save(new User("SAMPLE1", LocalDate.now(), USER_EMAIL2, ACTIVE_STATUS));
+      userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL3, ACTIVE_STATUS));
+      userRepository.save(new User("SAMPLE3", LocalDate.now(), USER_EMAIL4, ACTIVE_STATUS));
+      userRepository.flush();
 
-        int updatedUsersSize = userRepository.updateUserSetStatusForNameNativePostgres(INACTIVE_STATUS, "SAMPLE");
+      int updatedUsersSize = userRepository.updateUserSetStatusForNameNativePostgres(INACTIVE_STATUS, "SAMPLE");
 
-        assertThat(updatedUsersSize).isEqualTo(2);
-    }
+      assertThat(updatedUsersSize).isEqualTo(2);
+   }
 }
--- spring-data-modules/spring-data-jpa-enterprise-1/src/test/java/cn/tuyucheng/taketoday/boot/daos/UserRepositoryTCLiveTest.java
+++ spring-data-modules/spring-data-jpa-enterprise-1/src/test/java/cn/tuyucheng/taketoday/boot/daos/UserRepositoryTCLiveTest.java
@@ -1,57 +1,50 @@
 package cn.tuyucheng.taketoday.boot.daos;
 
-import cn.tuyucheng.taketoday.boot.Application;
-import cn.tuyucheng.taketoday.boot.domain.User;
-import org.junit.ClassRule;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
-import org.testcontainers.containers.PostgreSQLContainer;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = Application.class)
+import cn.tuyucheng.taketoday.boot.domain.User;
+
+@Testcontainers
+@SpringBootTest
 @ActiveProfiles("tc")
-@ContextConfiguration(initializers = {UserRepositoryTCLiveTest.Initializer.class})
-class UserRepositoryTCLiveTest extends UserRepositoryCommon {
+public class UserRepositoryTCLiveTest extends UserRepositoryCommon {
 
-    @ClassRule
-    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
-            .withDatabaseName("integration-tests-db")
-            .withUsername("sa")
-            .withPassword("sa");
+   @Container
+   public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
+         .withDatabaseName("integration-tests-db")
+         .withUsername("sa")
+         .withPassword("sa");
 
-    @Test
-    @Transactional
-    void givenUsersInDB_WhenUpdateStatusForNameModifyingQueryAnnotationNative_ThenModifyMatchingUsers() {
-        userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL, ACTIVE_STATUS));
-        userRepository.save(new User("SAMPLE1", LocalDate.now(), USER_EMAIL2, ACTIVE_STATUS));
-        userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL3, ACTIVE_STATUS));
-        userRepository.save(new User("SAMPLE3", LocalDate.now(), USER_EMAIL4, ACTIVE_STATUS));
-        userRepository.flush();
+   @Test
+   @Transactional
+   public void givenUsersInDB_WhenUpdateStatusForNameModifyingQueryAnnotationNative_ThenModifyMatchingUsers() {
+      userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL, ACTIVE_STATUS));
+      userRepository.save(new User("SAMPLE1", LocalDate.now(), USER_EMAIL2, ACTIVE_STATUS));
+      userRepository.save(new User("SAMPLE", LocalDate.now(), USER_EMAIL3, ACTIVE_STATUS));
+      userRepository.save(new User("SAMPLE3", LocalDate.now(), USER_EMAIL4, ACTIVE_STATUS));
+      userRepository.flush();
 
-        int updatedUsersSize = userRepository.updateUserSetStatusForNameNativePostgres(INACTIVE_STATUS, "SAMPLE");
+      int updatedUsersSize = userRepository.updateUserSetStatusForNameNativePostgres(INACTIVE_STATUS, "SAMPLE");
 
-        assertThat(updatedUsersSize).isEqualTo(2);
-    }
+      assertThat(updatedUsersSize).isEqualTo(2);
+   }
 
-    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
-            ).applyTo(configurableApplicationContext.getEnvironment());
-        }
-    }
+   @DynamicPropertySource
+   static void registerPgProperties(DynamicPropertyRegistry registry) {
+      registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+      registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+      registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+   }
 }
