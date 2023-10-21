c0a63774f017707d95ce2641a2776daca143107a
tuyucheng <tuyucheng2000@163.com>
2023-10-21 21:19:08 +0800

    ♻️ Article Code Matches first commit

--- java-core-modules/java-exceptions-1/src/main/java/cn/tuyucheng/taketoday/exceptions/throwvsthrows/Main.java
+++ java-core-modules/java-exceptions-1/src/main/java/cn/tuyucheng/taketoday/exceptions/throwvsthrows/Main.java
@@ -9,33 +9,39 @@ import java.net.SocketException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        TryCatch tryCatch = new TryCatch();
+   public static void main(String[] args) throws FileNotFoundException {
+      TryCatch tryCatch = new TryCatch();
 
-        try {
-            tryCatch.execute();
-        } catch (ConnectionException | SocketException ex) {
-            System.out.println("IOException");
-        } catch (Exception ex) {
-            System.out.println("General exception");
-        }
+      try {
+         tryCatch.execute();
+      } catch (ConnectionException | SocketException ex) {
+         System.out.println("IOException");
+      } catch (Exception ex) {
+         System.out.println("General exception");
+      }
 
-        checkedException();
-        checkedExceptionWithThrows();
-    }
+      checkedExceptionWithTryCatch();
+      checkedExceptionWithThrows();
+      divideByZero();
+   }
 
-    private static void checkedExceptionWithThrows() throws FileNotFoundException {
-        File file = new File("not_existing_file.txt");
-        FileInputStream stream = new FileInputStream(file);
-    }
+   private static void checkedExceptionWithThrows() throws FileNotFoundException {
+      File file = new File("not_existing_file.txt");
+      FileInputStream stream = new FileInputStream(file);
+   }
 
-    private static void checkedException() {
-        File file = new File("not_existing_file.txt");
-        try {
-            FileInputStream stream = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+   private static void checkedExceptionWithTryCatch() {
+      File file = new File("not_existing_file.txt");
+      try {
+         FileInputStream stream = new FileInputStream(file);
+      } catch (FileNotFoundException e) {
+         e.printStackTrace();
+      }
+   }
 
+   private static void divideByZero() {
+      int numerator = 1;
+      int denominator = 0;
+      int result = numerator / denominator;
+   }
 }
