db912ec20133d97e2a21300d6fa7a1861f81f65f
tuyucheng <tuyucheng2000@163.com>
2023-10-21 20:53:08 +0800

    ✨ Get file size efficiently in Java

--- java-core-modules/java-io-1/src/test/java/cn/tuyucheng/taketoday/size/JavaFileSizeUnitTest.java
+++ java-core-modules/java-io-1/src/test/java/cn/tuyucheng/taketoday/size/JavaFileSizeUnitTest.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,54 +16,72 @@ import java.nio.file.Paths;
 import static org.junit.Assert.assertEquals;
 
 public class JavaFileSizeUnitTest {
-	private static final long EXPECTED_FILE_SIZE_IN_BYTES = 11;
-	private String filePath;
+   private static final long EXPECTED_FILE_SIZE_IN_BYTES = 11;
+   private String filePath;
 
-	@Before
-	public void init() {
-		final String separator = File.separator;
-		filePath = String.join(separator, new String[]{"src", "test", "resources", "size", "sample_file_1.in"});
-	}
+   @Before
+   public void init() {
+      final String separator = File.separator;
+      filePath = String.join(separator, new String[]{"src", "test", "resources", "size", "sample_file_1.in"});
+   }
 
-	@Test
-	public void whenGetFileSize_thenCorrect() {
-		final File file = new File(filePath);
+   @Test
+   public void whenGetFileSize_thenCorrect() {
+      final File file = new File(filePath);
 
-		final long size = getFileSize(file);
+      final long size = getFileSize(file);
 
-		assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, size);
-	}
+      assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, size);
+   }
 
-	@Test
-	public void whenGetFileSizeUsingNioApi_thenCorrect() throws IOException {
-		final Path path = Paths.get(this.filePath);
-		final FileChannel fileChannel = FileChannel.open(path);
+   @Test
+   public void whenGetFileSizeUsingNioApi_thenCorrect() throws IOException {
+      final Path path = Paths.get(this.filePath);
+      final FileChannel fileChannel = FileChannel.open(path);
 
-		final long fileSize = fileChannel.size();
+      final long fileSize = fileChannel.size();
 
-		assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, fileSize);
-	}
+      assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, fileSize);
+   }
 
-	@Test
-	public void whenGetFileSizeUsingApacheCommonsIO_thenCorrect() {
-		final File file = new File(filePath);
+   @Test
+   public void whenGetFileSizeUsingApacheCommonsIO_thenCorrect() {
+      final File file = new File(filePath);
 
-		final long size = FileUtils.sizeOf(file);
+      final long size = FileUtils.sizeOf(file);
 
-		assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, size);
-	}
+      assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, size);
+   }
 
-	@Test
-	public void whenGetReadableFileSize_thenCorrect() {
-		final File file = new File(filePath);
+   @Test
+   public void whenGetReadableFileSize_thenCorrect() {
+      final File file = new File(filePath);
 
-		final long size = getFileSize(file);
+      final long size = getFileSize(file);
 
-		assertEquals(EXPECTED_FILE_SIZE_IN_BYTES + " bytes", FileUtils.byteCountToDisplaySize(size));
-	}
+      assertEquals(EXPECTED_FILE_SIZE_IN_BYTES + " bytes", FileUtils.byteCountToDisplaySize(size));
+   }
 
-	private long getFileSize(final File file) {
-		final long length = file.length();
-		return length;
-	}
+   private long getFileSize(final File file) {
+      final long length = file.length();
+      return length;
+   }
+
+   @Test
+   public void whenGetFileSizeUsingFileInputStream_thenCorrect() throws IOException {
+      try (FileInputStream fis = new FileInputStream(filePath)) {
+         long result = fis.getChannel().size();
+         assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, result);
+      }
+   }
+
+   @Test
+   public void whenGetFileSizeUsingUrlAndInputStream_thenCorrect() throws IOException {
+      File file = new File(filePath);
+      URL url = file.toURI().toURL();
+
+      try (InputStream stream = url.openStream()) {
+         assertEquals(EXPECTED_FILE_SIZE_IN_BYTES, stream.available());
+      }
+   }
 }
