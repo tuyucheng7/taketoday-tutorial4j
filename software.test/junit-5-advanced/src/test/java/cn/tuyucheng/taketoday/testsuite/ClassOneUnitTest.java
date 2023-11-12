bfadc24afee87fbac07b9fde937f3ba8b5f415f8
tuyucheng <tuyucheng2000@163.com>
2023-11-12 15:27:36 +0800

    ✨ add one extra method

--- software.test/junit-5-advanced/src/test/java/cn/tuyucheng/taketoday/testsuite/ClassOneUnitTest.java
+++ software.test/junit-5-advanced/src/test/java/cn/tuyucheng/taketoday/testsuite/ClassOneUnitTest.java
@@ -5,8 +5,13 @@ import org.junit.jupiter.api.Test;
 
 public class ClassOneUnitTest {
 
-	@Test
-	public void whenTrue_thenTrue() {
-		Assertions.assertTrue(true);
-	}
+   @Test
+   public void whenTrue_thenTrue() {
+      Assertions.assertTrue(true);
+   }
+
+   @Test
+   public void whenFalse_thenFalse() {
+      Assertions.assertFalse(false);
+   }
 }
--- software.test/junit-5-advanced/src/test/java/cn/tuyucheng/taketoday/testsuite/subpackage/ClassTwoUnitTest.java
+++ software.test/junit-5-advanced/src/test/java/cn/tuyucheng/taketoday/testsuite/subpackage/ClassTwoUnitTest.java
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ClassTwoUnitTest {
-	@Test
-	public void whenTrue_thenTrue() {
-		Assertions.assertTrue(true);
-	}
+   @Test
+   public void whenTrue_thenTrue() {
+      Assertions.assertTrue(true);
+   }
+
+   @Test
+   public void whenFalse_thenFalse() {
+      Assertions.assertFalse(false);
+   }
 }
