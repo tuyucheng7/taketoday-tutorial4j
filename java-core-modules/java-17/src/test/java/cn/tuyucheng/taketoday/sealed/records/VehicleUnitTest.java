c2f444e9b26978962c52d9b3c9207bfd03507761
tuyucheng <tuyucheng2000@163.com>
2023-11-18 13:32:22 +0800

    🐛 Fix VehicleUnitTest

--- java-core-modules/java-17/src/test/java/cn/tuyucheng/taketoday/sealed/records/VehicleUnitTest.java
+++ java-core-modules/java-17/src/test/java/cn/tuyucheng/taketoday/sealed/records/VehicleUnitTest.java
@@ -1,76 +1,75 @@
 package cn.tuyucheng.taketoday.sealed.records;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.lang.constant.ClassDesc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+public class VehicleUnitTest {
 
-class VehicleUnitTest {
+   private static Vehicle car;
+   private static Vehicle truck;
 
-	private static Vehicle car;
-	private static Vehicle truck;
+   @BeforeAll
+   public static void createInstances() {
+      car = new Car(4, "VZ500DA");
+      truck = new Truck(16000, "VZ600TA");
+   }
 
-	@BeforeAll
-	static void createInstances() {
-		car = new Car(4, "VZ500DA");
-		truck = new Truck(16000, "VZ600TA");
-	}
+   @Test
+   public void givenCar_whenUsingReflectionAPI_thenInterfaceIsSealed() throws ClassNotFoundException {
+      Assertions.assertThat(car.getClass().isSealed()).isEqualTo(false);
+      Assertions.assertThat(car.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
+      Assertions.assertThat(car.getClass().getInterfaces()[0].getPermittedSubclasses())
+            .contains(Class.forName(car.getClass().getCanonicalName()));
+   }
 
-	@Test
-	void givenCar_whenUsingReflectionAPI_thenInterfaceIsSealed() {
-		assertThat(car.getClass().isSealed()).isEqualTo(false);
-		assertThat(car.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
-		assertThat(Arrays.stream(car.getClass().getInterfaces()[0].getPermittedSubclasses()).map(Class::getName).toList())
-			.contains(car.getClass().getCanonicalName());
-	}
+   @Test
+   public void givenTruck_whenUsingReflectionAPI_thenInterfaceIsSealed() throws ClassNotFoundException {
+      Assertions.assertThat(truck.getClass().isSealed()).isEqualTo(false);
+      Assertions.assertThat(truck.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
+      Assertions.assertThat(truck.getClass().getInterfaces()[0].getPermittedSubclasses())
+            .contains(Class.forName(truck.getClass().getCanonicalName()));
+   }
 
-	@Test
-	void givenTruck_whenUsingReflectionAPI_thenInterfaceIsSealed() {
-		assertThat(truck.getClass().isSealed()).isEqualTo(false);
-		assertThat(truck.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
-		assertThat(Arrays.stream(truck.getClass().getInterfaces()[0].getPermittedSubclasses()).map(Class::getName).toList())
-			.contains(truck.getClass().getCanonicalName());
-	}
+   @Test
+   public void givenCar_whenGettingPropertyTraditionalWay_thenNumberOfSeatsPropertyIsReturned() {
+      Assertions.assertThat(getPropertyTraditionalWay(car)).isEqualTo(4);
+   }
 
-	@Test
-	void givenCar_whenGettingPropertyTraditionalWay_thenNumberOfSeatsPropertyIsReturned() {
-		assertThat(getPropertyTraditionalWay(car)).isEqualTo(4);
-	}
+   @Test
+   public void givenCar_whenGettingPropertyViaPatternMatching_thenNumberOfSeatsPropertyIsReturned() {
+      Assertions.assertThat(getPropertyViaPatternMatching(car)).isEqualTo(4);
+   }
 
-	@Test
-	void givenCar_whenGettingPropertyViaPatternMatching_thenNumberOfSeatsPropertyIsReturned() {
-		assertThat(getPropertyViaPatternMatching(car)).isEqualTo(4);
-	}
+   @Test
+   public void givenTruck_whenGettingPropertyTraditionalWay_thenLoadCapacityIsReturned() {
+      Assertions.assertThat(getPropertyTraditionalWay(truck)).isEqualTo(16000);
+   }
 
-	@Test
-	void givenTruck_whenGettingPropertyTraditionalWay_thenLoadCapacityIsReturned() {
-		assertThat(getPropertyTraditionalWay(truck)).isEqualTo(16000);
-	}
+   @Test
+   public void givenTruck_whenGettingPropertyViaPatternMatching_thenLoadCapacityIsReturned() {
+      Assertions.assertThat(getPropertyViaPatternMatching(truck)).isEqualTo(16000);
+   }
 
-	@Test
-	void givenTruck_whenGettingPropertyViaPatternMatching_thenLoadCapacityIsReturned() {
-		assertThat(getPropertyViaPatternMatching(truck)).isEqualTo(16000);
-	}
+   private int getPropertyTraditionalWay(Vehicle vehicle) {
+      if (vehicle instanceof Car) {
+         return ((Car) vehicle).getNumberOfSeats();
+      } else if (vehicle instanceof Truck) {
+         return ((Truck) vehicle).getLoadCapacity();
+      } else {
+         throw new RuntimeException("Unknown instance of Vehicle");
+      }
+   }
 
-	private int getPropertyTraditionalWay(Vehicle vehicle) {
-		if (vehicle instanceof Car) {
-			return ((Car) vehicle).getNumberOfSeats();
-		} else if (vehicle instanceof Truck) {
-			return ((Truck) vehicle).getLoadCapacity();
-		} else {
-			throw new RuntimeException("Unknown instance of Vehicle");
-		}
-	}
-
-	private int getPropertyViaPatternMatching(Vehicle vehicle) {
-		if (vehicle instanceof Car car) {
-			return car.getNumberOfSeats();
-		} else if (vehicle instanceof Truck truck) {
-			return truck.getLoadCapacity();
-		} else {
-			throw new RuntimeException("Unknown instance of Vehicle");
-		}
-	}
+   private int getPropertyViaPatternMatching(Vehicle vehicle) {
+      if (vehicle instanceof Car car) {
+         return car.getNumberOfSeats();
+      } else if (vehicle instanceof Truck truck) {
+         return truck.getLoadCapacity();
+      } else {
+         throw new RuntimeException("Unknown instance of Vehicle");
+      }
+   }
 }
