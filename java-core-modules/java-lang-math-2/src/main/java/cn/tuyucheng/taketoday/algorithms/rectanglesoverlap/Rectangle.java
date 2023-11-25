2a52219cd1ae497f494a79d736b2a2ceeddb28f9
tuyucheng <tuyucheng2000@163.com>
2023-11-25 21:41:04 +0800

    ⬆️ Update the RectangleOverlap adding a method without borders check.

--- java-core-modules/java-lang-math-2/src/main/java/cn/tuyucheng/taketoday/algorithms/rectanglesoverlap/Rectangle.java
+++ java-core-modules/java-lang-math-2/src/main/java/cn/tuyucheng/taketoday/algorithms/rectanglesoverlap/Rectangle.java
@@ -2,39 +2,51 @@ package cn.tuyucheng.taketoday.algorithms.rectanglesoverlap;
 
 public class Rectangle {
 
-	private Point bottomLeft;
-	private Point topRight;
+   private Point bottomLeft;
+   private Point topRight;
 
-	public Rectangle(Point bottomLeft, Point topRight) {
-		this.bottomLeft = bottomLeft;
-		this.topRight = topRight;
-	}
+   public Rectangle(Point bottomLeft, Point topRight) {
+      this.bottomLeft = bottomLeft;
+      this.topRight = topRight;
+   }
 
-	public Point getBottomLeft() {
-		return bottomLeft;
-	}
+   public Point getBottomLeft() {
+      return bottomLeft;
+   }
 
-	public void setBottomLeft(Point bottomLeft) {
-		this.bottomLeft = bottomLeft;
-	}
+   public void setBottomLeft(Point bottomLeft) {
+      this.bottomLeft = bottomLeft;
+   }
 
-	public Point getTopRight() {
-		return topRight;
-	}
+   public Point getTopRight() {
+      return topRight;
+   }
 
-	public void setTopRight(Point topRight) {
-		this.topRight = topRight;
-	}
+   public void setTopRight(Point topRight) {
+      this.topRight = topRight;
+   }
 
-	public boolean isOverlapping(Rectangle other) {
-		// one rectangle is to the top of the other
-		if (this.topRight.getY() < other.bottomLeft.getY() || this.bottomLeft.getY() > other.topRight.getY()) {
-			return false;
-		}
-		// one rectangle is to the left of the other
-		if (this.topRight.getX() < other.bottomLeft.getX() || this.bottomLeft.getX() > other.topRight.getX()) {
-			return false;
-		}
-		return true;
-	}
+   public boolean isOverlapping(Rectangle comparedRectangle) {
+      // one rectangle is to the top of the comparedRectangle
+      if (this.topRight.getY() < comparedRectangle.bottomLeft.getY() || this.bottomLeft.getY() > comparedRectangle.topRight.getY()) {
+         return false;
+      }
+      // one rectangle is to the left of the comparedRectangle
+      if (this.topRight.getX() < comparedRectangle.bottomLeft.getX() || this.bottomLeft.getX() > comparedRectangle.topRight.getX()) {
+         return false;
+      }
+      return true;
+   }
+
+   public boolean isOverlappingWithoutBorders(Rectangle comparedRectangle) {
+      // one rectangle is to the top of the comparedRectangle
+      if (this.topRight.getY() <= comparedRectangle.bottomLeft.getY() || this.bottomLeft.getY() >= comparedRectangle.topRight.getY()) {
+         return false;
+      }
+      // one rectangle is to the left of the comparedRectangle
+      if (this.topRight.getX() <= comparedRectangle.bottomLeft.getX() || this.bottomLeft.getX() >= comparedRectangle.topRight.getX()) {
+         return false;
+      }
+      return true;
+   }
 }
