378ae2c288bed2b1555202390262fb6434808a48
tuyucheng <tuyucheng2000@163.com>
2023-11-26 19:01:21 +0800

    🔥 remove spring boot credhub module

--- spring-boot-modules/spring-boot-credhub/README.md
+++
@@ -1,3 +0,0 @@
-## 相关文章
-
-+ [Spring CredHub指南](http://tu-yucheng.github.io/springboot/2023/05/11/spring-credhub.html)
--- spring-boot-modules/spring-boot-credhub/src/main/java/cn/tuyucheng/taketoday/OrderApplication.java
+++
@@ -1,7 +0,0 @@
-package cn.tuyucheng.taketoday;
-
-public class OrderApplication {
-   public static void main(String[] args) {
-      System.out.println("Hello world!");
-   }
-}
