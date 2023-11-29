eb787c91f5a9460856b28e1f0413b1faf3b189b4
tuyucheng <tuyucheng2000@163.com>
2023-11-29 10:52:46 +0800

    ♻️ removed deprecated WebSecurityConfigAdapter from security-modules/jjwt module

--- security-modules/jjwt/src/main/java/io/jsonwebtoken/jjwtfun/config/WebSecurityConfig.java
+++ security-modules/jjwt/src/main/java/io/jsonwebtoken/jjwtfun/config/WebSecurityConfig.java
@@ -4,9 +4,11 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.jjwtfun.service.SecretService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -21,20 +23,20 @@ import java.io.IOException;
 import java.util.Arrays;
 
 @Configuration
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Autowired
-    CsrfTokenRepository jwtCsrfTokenRepository;
+   @Autowired
+   private CsrfTokenRepository jwtCsrfTokenRepository;
 
-    @Autowired
-    SecretService secretService;
+   @Autowired
+   private SecretService secretService;
 
-    // ordered so we can use binary search below
-    private String[] ignoreCsrfAntMatchers = { "/dynamic-builder-compress", "/dynamic-builder-general", "/dynamic-builder-specific", "/set-secrets" };
+   // ordered so we can use binary search below
+   private final String[] ignoreCsrfAntMatchers = {"/dynamic-builder-compress", "/dynamic-builder-general", "/dynamic-builder-specific", "/set-secrets"};
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterAfter(new JwtCsrfValidatorFilter(), CsrfFilter.class)
+   @Bean
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      http.addFilterAfter(new JwtCsrfValidatorFilter(), CsrfFilter.class)
             .csrf()
             .csrfTokenRepository(jwtCsrfTokenRepository)
             .ignoringAntMatchers(ignoreCsrfAntMatchers)
@@ -42,38 +44,39 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/**")
             .permitAll();
-    }
 
-    private class JwtCsrfValidatorFilter extends OncePerRequestFilter {
+      return http.build();
+   }
 
-        @Override
-        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-            // NOTE: A real implementation should have a nonce cache so the token cannot be reused
+   private class JwtCsrfValidatorFilter extends OncePerRequestFilter {
+      @Override
+      protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+         // NOTE: A real implementation should have a nonce cache so the token cannot be reused
 
-            CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+         CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
 
-            if (
+         if (
             // only care if it's a POST
-            "POST".equals(request.getMethod()) &&
-            // ignore if the request path is in our list
-                Arrays.binarySearch(ignoreCsrfAntMatchers, request.getServletPath()) < 0 &&
-                // make sure we have a token
-                token != null) {
-                // CsrfFilter already made sure the token matched. Here, we'll make sure it's not expired
-                try {
-                    Jwts.parser()
-                        .setSigningKeyResolver(secretService.getSigningKeyResolver())
-                        .parseClaimsJws(token.getToken());
-                } catch (JwtException e) {
-                    // most likely an ExpiredJwtException, but this will handle any
-                    request.setAttribute("exception", e);
-                    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-                    RequestDispatcher dispatcher = request.getRequestDispatcher("expired-jwt");
-                    dispatcher.forward(request, response);
-                }
+               "POST".equals(request.getMethod()) &&
+                     // ignore if the request path is in our list
+                     Arrays.binarySearch(ignoreCsrfAntMatchers, request.getServletPath()) < 0 &&
+                     // make sure we have a token
+                     token != null) {
+            // CsrfFilter already made sure the token matched. Here, we'll make sure it's not expired
+            try {
+               Jwts.parser()
+                     .setSigningKeyResolver(secretService.getSigningKeyResolver())
+                     .parseClaimsJws(token.getToken());
+            } catch (JwtException e) {
+               // most likely an ExpiredJwtException, but this will handle any
+               request.setAttribute("exception", e);
+               response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+               RequestDispatcher dispatcher = request.getRequestDispatcher("expired-jwt");
+               dispatcher.forward(request, response);
             }
+         }
 
-            filterChain.doFilter(request, response);
-        }
-    }
+         filterChain.doFilter(request, response);
+      }
+   }
 }
