ba47f3bcdfa54a304a04486d22f3441de526096c
tuyucheng <tuyucheng2000@163.com>
2023-12-10 11:07:01 +0800

    ⬆️ Upgraded spring-session-jdbc to spring-boot-parent-3

--- spring-web-modules/spring-session/spring-session-jdbc/src/test/java/cn/tuyucheng/taketoday/springsessionjdbc/SpringSessionJdbcIntegrationTest.java
+++ spring-web-modules/spring-session/spring-session-jdbc/src/test/java/cn/tuyucheng/taketoday/springsessionjdbc/SpringSessionJdbcIntegrationTest.java
@@ -9,7 +9,7 @@ import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -29,63 +29,62 @@ import static org.assertj.core.api.Assertions.assertThat;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SpringSessionJdbcIntegrationTest {
 
-    @LocalServerPort
-    private int port;
+   @LocalServerPort
+   private int port;
 
-    @Autowired
-    private TestRestTemplate testRestTemplate;
+   @Autowired
+   private TestRestTemplate testRestTemplate;
 
-    @Before
-    public void setup() throws ClassNotFoundException {
-        Class.forName("org.h2.Driver");
-    }
+   @Before
+   public void setup() throws ClassNotFoundException {
+      Class.forName("org.h2.Driver");
+   }
 
-    @Test
-    public void givenApiHasStarted_whenH2DbIsQueried_thenSessionTablesAreEmpty() throws SQLException {
-        Assert.assertEquals(0, getSessionIdsFromDatabase().size());
-        Assert.assertEquals(0, getSessionAttributeBytesFromDatabase().size());
-    }
+   @Test
+   public void givenApiHasStarted_whenH2DbIsQueried_thenSessionTablesAreEmpty() throws SQLException {
+      Assert.assertEquals(0, getSessionIdsFromDatabase().size());
+      Assert.assertEquals(0, getSessionAttributeBytesFromDatabase().size());
+   }
 
-    @Test
-    public void givenGetInvoked_whenH2DbIsQueried_thenOneSessionIsCreated() throws SQLException {
-        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/", String.class)).isNotEmpty();
-        Assert.assertEquals(1, getSessionIdsFromDatabase().size());
-    }
+   @Test
+   public void givenGetInvoked_whenH2DbIsQueried_thenOneSessionIsCreated() throws SQLException {
+      assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/", String.class)).isNotEmpty();
+      Assert.assertEquals(1, getSessionIdsFromDatabase().size());
+   }
 
-    @Test
-    public void givenPostInvoked_whenH2DbIsQueried_thenSessionAttributeIsRetrieved() throws ClassNotFoundException, SQLException, IOException {
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("color", "red");
-        this.testRestTemplate.postForObject("http://localhost:" + port + "/saveColor", map, String.class);
-        List<byte[]> queryResponse = getSessionAttributeBytesFromDatabase();
-        Assert.assertEquals(1, queryResponse.size());
-        ObjectInput in = new ObjectInputStream(new ByteArrayInputStream(queryResponse.get(0)));
-        List<String> obj = (List<String>) in.readObject(); //Deserialize byte[] to object
-        Assert.assertEquals("red", obj.get(0));
-    }
+   @Test
+   public void givenPostInvoked_whenH2DbIsQueried_thenSessionAttributeIsRetrieved() throws ClassNotFoundException, SQLException, IOException {
+      MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+      map.add("color", "red");
+      this.testRestTemplate.postForObject("http://localhost:" + port + "/saveColor", map, String.class);
+      List<byte[]> queryResponse = getSessionAttributeBytesFromDatabase();
+      Assert.assertEquals(1, queryResponse.size());
+      ObjectInput in = new ObjectInputStream(new ByteArrayInputStream(queryResponse.get(0)));
+      List<String> obj = (List<String>) in.readObject(); // Deserialize byte[] to object
+      Assert.assertEquals("red", obj.get(0));
+   }
 
-    private List<String> getSessionIdsFromDatabase() throws SQLException {
-        List<String> result = new ArrayList<>();
-        ResultSet rs = getResultSet("SELECT * FROM SPRING_SESSION");
-        while (rs.next()) {
-            result.add(rs.getString("SESSION_ID"));
-        }
-        return result;
-    }
+   private List<String> getSessionIdsFromDatabase() throws SQLException {
+      List<String> result = new ArrayList<>();
+      ResultSet rs = getResultSet("SELECT * FROM SPRING_SESSION");
+      while (rs.next()) {
+         result.add(rs.getString("SESSION_ID"));
+      }
+      return result;
+   }
 
-    private List<byte[]> getSessionAttributeBytesFromDatabase() throws SQLException {
-        List<byte[]> result = new ArrayList<>();
-        ResultSet rs = getResultSet("SELECT * FROM SPRING_SESSION_ATTRIBUTES");
-        while (rs.next()) {
-            result.add(rs.getBytes("ATTRIBUTE_BYTES"));
-        }
-        return result;
-    }
+   private List<byte[]> getSessionAttributeBytesFromDatabase() throws SQLException {
+      List<byte[]> result = new ArrayList<>();
+      ResultSet rs = getResultSet("SELECT * FROM SPRING_SESSION_ATTRIBUTES");
+      while (rs.next()) {
+         result.add(rs.getBytes("ATTRIBUTE_BYTES"));
+      }
+      return result;
+   }
 
-    private ResultSet getResultSet(String sql) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
-        Statement stat = conn.createStatement();
-        return stat.executeQuery(sql);
-    }
-
+   private ResultSet getResultSet(String sql) throws SQLException {
+      Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+      Statement stat = conn.createStatement();
+      return stat.executeQuery(sql);
+   }
 }
