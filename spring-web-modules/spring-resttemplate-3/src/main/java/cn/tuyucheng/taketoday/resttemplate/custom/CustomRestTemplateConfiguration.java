89bba7b6be3305c65392c79cf7360ebbf19f0d0d
tuyucheng <tuyucheng2000@163.com>
2023-12-21 16:30:50 +0800

    ⬆️ Made changes to upgrade the module spring-resttemplate-3 to Spring Boot 3

--- spring-web-modules/spring-resttemplate-3/src/main/java/cn/tuyucheng/taketoday/resttemplate/custom/CustomRestTemplateConfiguration.java
+++ spring-web-modules/spring-resttemplate-3/src/main/java/cn/tuyucheng/taketoday/resttemplate/custom/CustomRestTemplateConfiguration.java
@@ -1,9 +1,20 @@
 package cn.tuyucheng.taketoday.resttemplate.custom;
 
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.ssl.SSLContextBuilder;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,33 +23,28 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-
 @Configuration
 public class CustomRestTemplateConfiguration {
 
-    @Value("${trust.store}")
-    private Resource trustStore;
+   @Value("${trust.store}")
+   private Resource trustStore;
 
-    @Value("${trust.store.password}")
-    private String trustStorePassword;
+   @Value("${trust.store.password}")
+   private String trustStorePassword;
 
-    @Bean
-    public RestTemplate restTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException,
-          CertificateException, MalformedURLException, IOException {
+   @Bean
+   public RestTemplate restTemplate()
+         throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException, MalformedURLException, IOException {
+      SSLContext sslContext = new SSLContextBuilder()
+            .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
+      SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
 
-        SSLContext sslContext = new SSLContextBuilder()
-              .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
-        SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
+      final HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+            .setSSLSocketFactory(sslConFactory)
+            .build();
 
-        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslConFactory).build();
-        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        return new RestTemplate(requestFactory);
-    }
+      CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
+      ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+      return new RestTemplate(requestFactory);
+   }
 }
