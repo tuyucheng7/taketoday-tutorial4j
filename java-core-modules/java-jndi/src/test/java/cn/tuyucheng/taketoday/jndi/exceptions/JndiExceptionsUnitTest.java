b4cf8fb6fefdcd78c9aa3178b87d732a1c929f86
tuyucheng <tuyucheng2000@163.com>
2024-01-05 18:10:55 +0800

    🔨 Fix failing module core-java-modules - upgrade to JDK 21

--- java-core-modules/java-jndi/src/test/java/cn/tuyucheng/taketoday/jndi/exceptions/JndiExceptionsUnitTest.java
+++ java-core-modules/java-jndi/src/test/java/cn/tuyucheng/taketoday/jndi/exceptions/JndiExceptionsUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.jndi.JndiTemplate;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.mock.jndi.*;
 
 import javax.naming.InitialContext;
 import javax.naming.NameNotFoundException;
