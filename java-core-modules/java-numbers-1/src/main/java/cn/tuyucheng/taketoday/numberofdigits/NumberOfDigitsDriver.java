42473c0668df1635649519b19297a6ff60c0895c
taketoday <tuyucheng2000@163.com>
2024-01-09 17:34:50 +0800

    ➖ Remove explicit logging dependency

--- java-core-modules/java-numbers-1/src/main/java/cn/tuyucheng/taketoday/numberofdigits/NumberOfDigitsDriver.java
+++ java-core-modules/java-numbers-1/src/main/java/cn/tuyucheng/taketoday/numberofdigits/NumberOfDigitsDriver.java
@@ -1,35 +1,35 @@
 package cn.tuyucheng.taketoday.numberofdigits;
 
-import org.apache.log4j.Logger;
+import java.util.logging.Logger;
 
 public class NumberOfDigitsDriver {
-	private static NumberOfDigits numberOfDigits;
+   private static NumberOfDigits numberOfDigits;
 
-	private static Logger LOG = Logger.getLogger(NumberOfDigitsDriver.class);
+   private static Logger LOG = Logger.getLogger(NumberOfDigitsDriver.class.getName());
 
-	static {
-		numberOfDigits = new NumberOfDigits();
-	}
+   static {
+      numberOfDigits = new NumberOfDigits();
+   }
 
-	public static void main(String[] args) {
-		LOG.info("Testing all methods...");
+   public static void main(String[] args) {
+      LOG.info("Testing all methods...");
 
-		long length = numberOfDigits.stringBasedSolution(602);
-		LOG.info("String Based Solution : " + length);
+      long length = numberOfDigits.stringBasedSolution(602);
+      LOG.info("String Based Solution : " + length);
 
-		length = numberOfDigits.logarithmicApproach(602);
-		LOG.info("Logarithmic Approach : " + length);
+      length = numberOfDigits.logarithmicApproach(602);
+      LOG.info("Logarithmic Approach : " + length);
 
-		length = numberOfDigits.repeatedMultiplication(602);
-		LOG.info("Repeated Multiplication : " + length);
+      length = numberOfDigits.repeatedMultiplication(602);
+      LOG.info("Repeated Multiplication : " + length);
 
-		length = numberOfDigits.shiftOperators(602);
-		LOG.info("Shift Operators : " + length);
+      length = numberOfDigits.shiftOperators(602);
+      LOG.info("Shift Operators : " + length);
 
-		length = numberOfDigits.dividingWithPowersOf2(602);
-		LOG.info("Dividing with Powers of 2 : " + length);
+      length = numberOfDigits.dividingWithPowersOf2(602);
+      LOG.info("Dividing with Powers of 2 : " + length);
 
-		length = numberOfDigits.divideAndConquer(602);
-		LOG.info("Divide And Conquer : " + length);
-	}
+      length = numberOfDigits.divideAndConquer(602);
+      LOG.info("Divide And Conquer : " + length);
+   }
 }
