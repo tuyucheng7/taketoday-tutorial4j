2c2ac79a82ac3c78d61ad0fda87e32c4fe43398c
taketoday <tuyucheng2000@163.com>
2024-01-25 20:09:48 +0800

    ⬆️ Upgrade spring-resttemplate-1 module to Spring Boot 3

--- spring-web-modules/spring-resttemplate-1/src/main/java/cn/tuyucheng/taketoday/resttemplate/web/handler/RestTemplateResponseErrorHandler.java
+++ spring-web-modules/spring-resttemplate-1/src/main/java/cn/tuyucheng/taketoday/resttemplate/web/handler/RestTemplateResponseErrorHandler.java
@@ -1,45 +1,37 @@
 package cn.tuyucheng.taketoday.resttemplate.web.handler;
 
-import cn.tuyucheng.taketoday.resttemplate.web.exception.NotFoundException;
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResponseErrorHandler;
 
-import java.io.IOException;
+import cn.tuyucheng.taketoday.resttemplate.web.exception.NotFoundException;
 
 @Component
-public class RestTemplateResponseErrorHandler
-      implements ResponseErrorHandler {
+public class RestTemplateResponseErrorHandler implements ResponseErrorHandler {
 
-    @Override
-    public boolean hasError(ClientHttpResponse httpResponse)
-          throws IOException {
+   @Override
+   public boolean hasError(ClientHttpResponse httpResponse) throws IOException {
+      return httpResponse.getStatusCode()
+            .is5xxServerError() || httpResponse.getStatusCode()
+            .is4xxClientError();
+   }
 
-        return (httpResponse
-              .getStatusCode()
-              .series() == HttpStatus.Series.CLIENT_ERROR || httpResponse
-              .getStatusCode()
-              .series() == HttpStatus.Series.SERVER_ERROR);
-    }
-
-    @Override
-    public void handleError(ClientHttpResponse httpResponse)
-          throws IOException {
-
-        if (httpResponse
-              .getStatusCode()
-              .series() == HttpStatus.Series.SERVER_ERROR) {
-            // Handle SERVER_ERROR
-            throw new HttpClientErrorException(httpResponse.getStatusCode());
-        } else if (httpResponse
-              .getStatusCode()
-              .series() == HttpStatus.Series.CLIENT_ERROR) {
-            // Handle CLIENT_ERROR
-            if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
-                throw new NotFoundException();
-            }
-        }
-    }
+   @Override
+   public void handleError(ClientHttpResponse httpResponse) throws IOException {
+      if (httpResponse.getStatusCode()
+            .is5xxServerError()) {
+         // Handle SERVER_ERROR
+         throw new HttpClientErrorException(httpResponse.getStatusCode());
+      } else if (httpResponse.getStatusCode()
+            .is4xxClientError()) {
+         // Handle CLIENT_ERROR
+         if (httpResponse.getStatusCode() == HttpStatus.NOT_FOUND) {
+            throw new NotFoundException();
+         }
+      }
+   }
 }
