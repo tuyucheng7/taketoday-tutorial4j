d9fda6ae3932cde25d3731df8c01f2c8d54dc0d1
taketoday <tuyucheng2000@163.com>
2024-03-09 14:37:30 +0800

    ♻️ Improvement to Potential issue in Functional Programming in Java

--- java-core-modules/java-functional/src/main/java/cn/tuyucheng/taketoday/functional/Currying.java
+++ java-core-modules/java-functional/src/main/java/cn/tuyucheng/taketoday/functional/Currying.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 
 public class Currying {
 
-   private static Function<Double, Function<Double, Double>> weight = mass -> gravity -> mass * gravity;
+   private static Function<Double, Function<Double, Double>> weight = gravity -> mass -> mass * gravity;
 
    private static Function<Double, Double> weightOnEarth = weight.apply(9.81);
 
@@ -22,5 +22,4 @@ public class Currying {
       final double gravity = 9.81;
       return mass -> mass * gravity;
    }
-
 }
