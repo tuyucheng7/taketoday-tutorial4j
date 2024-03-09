f99713c9cb677494af0d407583775a7f13fdea5f
tuyucheng <tuyucheng2000@163.com>
2024-03-09 13:16:48 +0800

    ♻️ Remove spring-testing-1 dependency on the parent POM and fix the dependencies to fix the integration tests

--- software.test/spring-testing-1/src/main/java/cn/tuyucheng/taketoday/config/WebConfig.java
+++ software.test/spring-testing-1/src/main/java/cn/tuyucheng/taketoday/config/WebConfig.java
@@ -1,11 +1,10 @@
 package cn.tuyucheng.taketoday.config;
 
+import jakarta.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.servlet.ServletContext;
 
 @EnableWebMvc
 @Configuration
