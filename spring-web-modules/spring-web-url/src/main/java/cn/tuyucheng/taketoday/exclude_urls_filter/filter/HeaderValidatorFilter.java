38b150b1a976112b6cc660d8e0ab4cf438c50958
taketoday <tuyucheng2000@163.com>
2024-03-20 19:54:11 +0800

    ⬆️ Upgrade spring-web-url to Spring Boot 3

--- spring-web-modules/spring-web-url/src/main/java/cn/tuyucheng/taketoday/exclude_urls_filter/filter/HeaderValidatorFilter.java
+++ spring-web-modules/spring-web-url/src/main/java/cn/tuyucheng/taketoday/exclude_urls_filter/filter/HeaderValidatorFilter.java
@@ -4,31 +4,31 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @Order(1)
 public class HeaderValidatorFilter extends OncePerRequestFilter {
-    @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
-          throws ServletException,
-          IOException {
-        String countryCode = request.getHeader("X-Country-Code");
-        if (!"US".equals(countryCode)) {
-            response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid Locale");
-            return;
-        }
-        filterChain.doFilter(request, response);
-    }
+   @Override
+   protected void doFilterInternal(HttpServletRequest request,
+                                   HttpServletResponse response,
+                                   FilterChain filterChain)
+         throws ServletException,
+         IOException {
+      String countryCode = request.getHeader("X-Country-Code");
+      if (!"US".equals(countryCode)) {
+         response.sendError(HttpStatus.BAD_REQUEST.value(), "Invalid Locale");
+         return;
+      }
+      filterChain.doFilter(request, response);
+   }
 
-    @Override
-    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
-        String path = request.getRequestURI();
-        return "/health".equals(path);
-    }
+   @Override
+   protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+      String path = request.getRequestURI();
+      return "/health".equals(path);
+   }
 }
--- spring-web-modules/spring-web-url/src/main/java/cn/tuyucheng/taketoday/exclude_urls_filter/filter/LogFilter.java
+++ spring-web-modules/spring-web-url/src/main/java/cn/tuyucheng/taketoday/exclude_urls_filter/filter/LogFilter.java
@@ -5,22 +5,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @Order(1)
 public class LogFilter extends OncePerRequestFilter {
-    private final Logger logger = LoggerFactory.getLogger(LogFilter.class);
+   private final Logger logger = LoggerFactory.getLogger(LogFilter.class);
 
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-        String path = request.getRequestURI();
-        String contentType = request.getContentType();
-        logger.info("Request URL path : {}, Request content type: {}", path, contentType);
-        filterChain.doFilter(request, response);
-    }
+   @Override
+   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                   FilterChain filterChain) throws ServletException, IOException {
+      String path = request.getRequestURI();
+      String contentType = request.getContentType();
+      logger.info("Request URL path : {}, Request content type: {}", path, contentType);
+      filterChain.doFilter(request, response);
+   }
 }
